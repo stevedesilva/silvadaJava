87b6538f3a38223a6e08a14943a909b6726fb898
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2021-09-08 20:24:22 +0100

    java capitalization

--- src/test/java/dsa/section10/CapitalisationTest.java
+++ src/test/java/dsa/section10/CapitalisationTest.java
@@ -8,18 +8,19 @@ import org.junit.jupiter.params.provider.CsvSource;
 class CapitalisationTest {
     @ParameterizedTest
     @CsvSource({"A, A", "a,A", "ab, Ab", "abc, Abc", "abcd efg, Abcd Efg"})
-    public void shouldCapitalise(String input, String expected) {
-        Capitalisation capitalisation = new Capitalisation();
-        String actual = capitalisation.capitaliseFirstWordInSentence(input);
-        Assertions.assertEquals(expected,actual);
+    public void shouldCapitaliseWithSpecialCharacters(String input, String expected) {
+        Assertions.assertEquals(expected, new Capitalisation().capitaliseFirstWordInSentenceUsingByteArray(input));
+        Assertions.assertEquals(expected, new Capitalisation().capitaliseFirstCharInWordUsingCharArray(input));
+        Assertions.assertEquals(expected, new Capitalisation().capitaliseFirstCharInWordUsingSubstring(input));
     }
 
     @Test
     public void shouldCapitaliseWithSpecialCharacters() {
         String sentence = "look, it is not working!";
         String expected = "Look, It Is Not Working!";
-        Capitalisation capitalisation = new Capitalisation();
-        String actual = capitalisation.capitaliseFirstWordInSentence(sentence);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, new Capitalisation().capitaliseFirstWordInSentenceUsingByteArray(sentence));
+        Assertions.assertEquals(expected, new Capitalisation().capitaliseFirstCharInWordUsingCharArray(sentence));
+        Assertions.assertEquals(expected, new Capitalisation().capitaliseFirstCharInWordUsingSubstring(sentence));
     }
+
 }
