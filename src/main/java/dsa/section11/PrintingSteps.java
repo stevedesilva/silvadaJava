c20d84cb601a6f4d530f9ca3dfc951b3f442229f
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2021-09-13 23:01:55 +0100

    refactor adding naive method

--- src/main/java/dsa/section11/PrintingSteps.java
+++ src/main/java/dsa/section11/PrintingSteps.java
@@ -1,20 +1,19 @@
 package dsa.section11;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PrintingSteps {
-
 
     public List<String> executeWithList(int num) {
         List<String> steps= new ArrayList<>();
         for (int i = 1; i <= num; i++) {
 
-            String stepsAndSpaces = String.format("%s%s\n", "#".repeat(i), (" ").repeat(num - i));
+            String stepsAndSpaces = String.format("%s%s", "#".repeat(i), (" ").repeat(num - i));
             steps.add(stepsAndSpaces);
-            System.out.print(stepsAndSpaces);
-
         }
+        printSteps(steps.toArray(new String[steps.size()]));
         return steps;
     }
 
@@ -22,11 +21,34 @@ public class PrintingSteps {
         String[] result = new String[num];
         for (int i = 0; i < num; i++) {
             int count = i + 1;
-            String stepsAndSpaces = String.format("%s%s\n", "#".repeat(count), (" ").repeat(num  - count));
+            String stepsAndSpaces = String.format("%s%s", "#".repeat(count), (" ").repeat(num  - count));
             result[i]=stepsAndSpaces;
-            System.out.print(stepsAndSpaces);
         }
+        printSteps(result);
         return result;
+    }
+
+    public String[] executeWithArrayNaive(int num) {
+        String[] result = new String[num];
+        for (int row = 0; row < num; row++) {
+            String[] colRes = new String[num];
+            for (int col = 0; col < num; col++) {
+                if (col <= row) {
+                    colRes[col] = "#";
+                } else {
+                    colRes[col] = " ";
+                }
+            }
+            result[row] = String.join("",colRes);
+        }
+        printSteps(result);
+        return result;
+    }
+
+    private void printSteps(String[] steps){
+        for (int i = 0; i < steps.length; i++) {
+            System.out.println(steps[i]);
+        }
     }
 
 }
--- src/test/java/dsa/section11/PrintingStepsTest.java
+++ src/test/java/dsa/section11/PrintingStepsTest.java
@@ -9,33 +9,38 @@ class PrintingStepsTest {
 
     @Test
     public void noStep(){
-        Assertions.assertArrayEquals(new PrintingSteps().executeWithArray(1),new String[]{"#\n",});
-        Assertions.assertEquals(List.of("#\n"), new PrintingSteps().executeWithList(1));
+        Assertions.assertArrayEquals(new PrintingSteps().executeWithArray(0),new String[]{});
+        Assertions.assertEquals(List.of(), new PrintingSteps().executeWithList(0));
+        Assertions.assertArrayEquals(new PrintingSteps().executeWithArrayNaive(0),new String[]{});
     }
 
     @Test
     public void oneStep(){
-        Assertions.assertArrayEquals(new PrintingSteps().executeWithArray(1),new String[]{"#\n",});
-        Assertions.assertEquals(List.of("#\n"), new PrintingSteps().executeWithList(1));
+        Assertions.assertArrayEquals(new PrintingSteps().executeWithArray(1),new String[]{"#",});
+        Assertions.assertArrayEquals(new PrintingSteps().executeWithArrayNaive(1),new String[]{"#",});
+        Assertions.assertEquals(List.of("#"), new PrintingSteps().executeWithList(1));
     }
 
 
     @Test
     public void twoStep(){
-        Assertions.assertArrayEquals(new PrintingSteps().executeWithArray(2),new String[]{"# \n","##\n"});
-        Assertions.assertEquals(List.of("# \n","##\n"), new PrintingSteps().executeWithList(2));
+        Assertions.assertArrayEquals(new PrintingSteps().executeWithArray(2),new String[]{"# ","##"});
+        Assertions.assertArrayEquals(new PrintingSteps().executeWithArrayNaive(2),new String[]{"# ","##"});
+        Assertions.assertEquals(List.of("# ","##"), new PrintingSteps().executeWithList(2));
     }
 
     @Test
     public void threeStep(){
-        Assertions.assertArrayEquals(new PrintingSteps().executeWithArray(3),new String[]{"#  \n","## \n","###\n"});
-        Assertions.assertEquals(List.of("#  \n","## \n","###\n"), new PrintingSteps().executeWithList(3));
+        Assertions.assertArrayEquals(new PrintingSteps().executeWithArray(3),new String[]{"#  ","## ","###"});
+        Assertions.assertArrayEquals(new PrintingSteps().executeWithArrayNaive(3),new String[]{"#  ","## ","###"});
+        Assertions.assertEquals(List.of("#  ","## ","###"), new PrintingSteps().executeWithList(3));
     }
 
     @Test
     public void fourStep(){
-        Assertions.assertArrayEquals(new PrintingSteps().executeWithArray(4),new String[]{"#   \n","##  \n","### \n","####\n"});
-        Assertions.assertEquals(List.of("#   \n","##  \n","### \n","####\n"), new PrintingSteps().executeWithList(4));
+        Assertions.assertArrayEquals(new PrintingSteps().executeWithArrayNaive(4),new String[]{"#   ","##  ","### ","####"});
+        Assertions.assertArrayEquals(new PrintingSteps().executeWithArray(4),new String[]{"#   ","##  ","### ","####"});
+        Assertions.assertEquals(List.of("#   ","##  ","### ","####"), new PrintingSteps().executeWithList(4));
     }
 
 }
