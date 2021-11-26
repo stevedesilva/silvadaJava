c223b6e51a2a83b9035338d498eebc5d4d744ae5
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2021-11-26 08:50:55 +0000

    add recursive call

--- src/main/java/dsa/section15/Fibonacci.java
+++ src/main/java/dsa/section15/Fibonacci.java
@@ -6,16 +6,38 @@ import java.util.stream.Stream;
 import static java.util.stream.Collectors.toList;
 
 public class Fibonacci {
+
+    // 0 , 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
     public int fibonacciSequenceAsArray(int n) {
-        if ( n <= 2) {
-            return 1;
+        // add extraSpaceFor initial entries 0,1 - handles 0 being passed in
+        // if space uses exactly what is needed
+        int[] sequence = new int[n + 2];
+        sequence[0]=0;
+        sequence[1]=1;
+
+        for (int i = 2; i <= n; i++) {
+            sequence[i] = sequence[i - 1] + sequence[i - 2];
         }
+        return sequence[n];
+    }
+    // 0 , 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+    // fib is 4 = 3 (zero indexed)
+    // fib us 3 = 2 (zero indexed)
+    public int fibonacciSequenceAsArrayAlternative(int n) {
+        // deal with 0 and 1
+        if ( n < 2) {
+            return n;
+        }
+        // n is zero indexed so add additional space
         int max = n + 1;
         int[] sequence = new int[max];
         sequence[0]=0;
         sequence[1]=1;
 
-        for (int i = 2; i < max; i++) {
+
+        // array index 0 1 2 3 ..
+        // start at 2 index in array (i = 2) - if n = 2 then
+        for (int i = 2; i <= n; i++) {
             int value = sequence[i - 1] + sequence[i - 2];
             sequence[i] = value;
         }
@@ -24,6 +46,9 @@ public class Fibonacci {
 
     // 0 , 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
     public int fibonacciSequenceAsList(int n) {
+        if ( n < 2) {
+            return n;
+        }
         List<Integer> sequence = Stream.of(0,1).collect(toList());
 
         for ( int i = 2; i <= n; i++) {
@@ -36,8 +61,7 @@ public class Fibonacci {
 
     public int fibonacciSequenceRecursiveList(int num) {
         List<Integer> result = fibonacciSequenceRecursiveList(num, 2, Stream.of(0,1).collect(toList()));
-        int lastIdx = result.size() - 1;
-        return result.get(lastIdx);
+        return result.get(num);
     }
 
     private List<Integer> fibonacciSequenceRecursiveList(int num, int count, List<Integer> result) {
@@ -48,26 +72,29 @@ public class Fibonacci {
         return fibonacciSequenceRecursiveList(num, ++count, result);
     }
 
-    public int fibonacciSequenceRecursiveArray(int num) {
-        if (num <= 2) return 1;
-
-        final int max = num + 1;
-        int[] sequence  = new int[max];
+    public int fibonacciSequenceSpaceOfNRecursiveArray(int num) {
+        int[] sequence  = new int[num + 2];
         sequence[0] = 0;
         sequence[1] = 1;
 
-        int[] result = fibonacciSequenceRecursiveArray(num, 2, sequence);
-        int lastIdx = result.length - 1;
-        return result[lastIdx];
+        int[] result = fibonacciSequenceSpaceOfNRecursiveArray(num, 2, sequence);
+        return result[num];
     }
 
-    private int[] fibonacciSequenceRecursiveArray(int num, int count, int[] result) {
+    private int[] fibonacciSequenceSpaceOfNRecursiveArray(int num, int count, int[] result) {
         if (count > num) {
             return result;
         }
         result[count] = result[count - 1] + result[count - 2];
-        return fibonacciSequenceRecursiveArray(num, ++count, result);
+        return fibonacciSequenceSpaceOfNRecursiveArray(num, ++count, result);
     }
 
+    //    fibonacciConstantSpaceRecursive
+    public int fibRecursive(int n) {
+        if (n < 2) {
+            return n;
+        }
+        return fibRecursive(n - 1) + fibRecursive(n - 2);
+    }
 
 }
--- src/test/java/dsa/section15/FibonacciTest.java
+++ src/test/java/dsa/section15/FibonacciTest.java
@@ -9,13 +9,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FibonacciTest {
     @ParameterizedTest
-    @CsvSource({"0,1","1,1","2,1","3,2", "4,3", "5,5", "6,8", "7,13", "8,21", "9,34", "10,55"})
+    @CsvSource({"0,0","1,1","2,1","3,2", "4,3", "5,5", "6,8", "7,13", "8,21", "9,34", "10,55"})
     public void ShouldReturnFibonacciNumber(int input, int expected
     ) {
-//        assertEquals(expected, new Fibonacci().fibonacciSequenceAsList(input));
-//        assertEquals(expected, new Fibonacci().fibonacciSequenceAsArray(input));
-//        assertEquals(expected, new Fibonacci().fibonacciSequenceRecursiveList(input));
-        assertEquals(expected, new Fibonacci().fibonacciSequenceRecursiveArray(input));
+        assertEquals(expected, new Fibonacci().fibonacciSequenceAsList(input));
+        assertEquals(expected, new Fibonacci().fibonacciSequenceAsArray(input));
+        assertEquals(expected, new Fibonacci().fibonacciSequenceAsArrayAlternative(input));
+        assertEquals(expected, new Fibonacci().fibonacciSequenceRecursiveList(input));
+        assertEquals(expected, new Fibonacci().fibonacciSequenceSpaceOfNRecursiveArray(input));
+        assertEquals(expected, new Fibonacci().fibRecursive(input));
     }
 
 }
