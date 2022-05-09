675468a2a33a04e7bf4bd19868f2393dc1956d86
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-05-09 20:17:55 +0100

    clean up

--- src/main/java/lambda/function/executor/Executor.java
+++ src/main/java/lambda/function/executor/Executor.java
@@ -3,8 +3,8 @@ package lambda.function.executor;
 import java.util.Set;
 import java.util.function.Function;
 
-public interface Executor<T,R> {
-    void addFunctions(Function<T,R>... data);
-    void addData(T... data);
-    Set<R> execute();
+public interface Executor{
+    <T> void addFunctions(Function<T,T>... data);
+    <T> void addData(T... data);
+    <T> Set<T> execute();
 }
--- src/test/java/patterns/command/lambda/generic/executor/OperationExecutorTest.java
+++ src/test/java/patterns/command/lambda/generic/executor/OperationExecutorTest.java
@@ -3,15 +3,21 @@ package patterns.command.lambda.generic.executor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
+import java.util.List;
 
 class OperationExecutorTest {
     @Test
     public void shouldExecuteFileOperationsLambda() {
         OperationExecutor executor = new OperationExecutor();
-        Assertions.assertEquals("Opening file file1", executor.executeOperation(()-> "Opening file file1" ));
-        Assertions.assertEquals("Saving file file1", executor.executeOperation(()-> "Saving file file1" ));
-        Assertions.assertEquals(1, executor.executeOperation(()-> 1 ));
-    }
+        Assertions.assertEquals("Opening file file1", executor.executeOperation(() -> "Opening file file1"));
+        Assertions.assertEquals("Saving file file1", executor.executeOperation(() -> "Saving file file1"));
+        Assertions.assertEquals(1, executor.executeOperation(() -> 1));
+        Assertions.assertEquals(19.0, executor.executeOperation(() -> 19.0));
+        Assertions.assertEquals(true, executor.executeOperation(() -> 11 == 11));
 
+        final List<GenericOperation> operations = executor.operations;
+        Assertions.assertEquals(5, operations.size());
+        operations.forEach(o -> System.out.println(o.execute()));
+
+    }
 }
