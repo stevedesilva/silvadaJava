6219be8bd972c3c227ea1a0a41c3f7f7ebe0cf45
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-05-21 12:13:40 +0100

    Refactor to monorepo

--- local_test.sh
+++ local_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "building..."
-docker build -t lazy-scheduler -f katas/Dockerfile .
+docker build -t my-repos -f Dockerfile .
 echo "running..."
 #docker run -it --rm -v $(pwd):/build -w /build --name test-lazy-scheduler lazy-scheduler
-docker run -it --rm --name test-lazy-scheduler lazy-scheduler bash
+docker run -it --rm --name my-repos my-repos
