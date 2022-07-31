3852d19d66d962e49077b843ce97a593fddbe2c1
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-07-31 12:14:40 +0100

    Add leap year calculator

--- dsa/src/main/java/commonsense/leapyear/LeapYear.java
+++ dsa/src/main/java/commonsense/leapyear/LeapYear.java
@@ -1,6 +1,11 @@
 package commonsense.leapyear;
 
 public class LeapYear {
+    /*
+    yes	Leap Years are any year that can be exactly divided by 4 (such as 2016, 2020, 2024, etc)
+ 	not	except if it can be exactly divided by 100, then it isn't (such as 2100, 2200, etc)
+ 	 	yes	except if it can be exactly divided by 400, then it is (such as 2000, 2400)
+     */
     public static boolean isLeapYear(int year) {
         if (year % 4 != 0) {
             return false;
--- dsa/src/test/java/commonsense/leapyear/LeapYearTest.java
+++ dsa/src/test/java/commonsense/leapyear/LeapYearTest.java
@@ -5,10 +5,22 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+
 class LeapYearTest {
     @ParameterizedTest
     @CsvSource({"2016, true", "2024, true","2028, true","2032, true", "2031, false","2033, false"})
-    public void ShouldReturnTrueWhenLeapYear(int input, boolean result) {
+    public void ShouldReturnTrueWhenLeapYearExactlyDivisibleByFour(int input, boolean result) {
+        MatcherAssert.assertThat(LeapYear.isLeapYear(input), Matchers.equalTo(result));
+    }
+    @ParameterizedTest
+    @CsvSource({"1600, true","2400, true"})
+    public void ShouldReturnTrueWhenLeapYearExactlyDivisibleBy400(int input, boolean result) {
+        MatcherAssert.assertThat(LeapYear.isLeapYear(input), Matchers.equalTo(result));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"2100, false","2200, false"})
+    public void ShouldReturnTrueWhenLeapYearExactlyDivisibleBy100(int input, boolean result) {
         MatcherAssert.assertThat(LeapYear.isLeapYear(input), Matchers.equalTo(result));
     }
 }
