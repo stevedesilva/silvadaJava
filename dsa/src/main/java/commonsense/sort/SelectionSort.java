9ba54983c52d8b40bdcc636ef245969387ef7af3
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-11 21:03:03 +0100

    add impl of selection sort

--- dsa/src/main/java/commonsense/sort/SelectionSort.java
+++ dsa/src/main/java/commonsense/sort/SelectionSort.java
@@ -2,6 +2,19 @@ package commonsense.sort;
 
 public class SelectionSort {
     public int[] sort(int[] array) {
+        for(int i=0; i < array.length; i++) {
+            int lowest = i;
+            for(int j=i+1; j < array.length; j++) {
+                if (array[j] < array[lowest]) {
+                    lowest = j;
+                }
+            }
+            if (lowest != i) {
+                int temp = array[i];
+                array[i] = array[lowest];
+                array[lowest] = temp;
+            }
+        }
         return array;
     }
 }
