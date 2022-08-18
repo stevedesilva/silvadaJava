201c108505a183d6cea833b74b91c0f29c4e6a67
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-18 05:12:49 +0100

    Add InsertionSort impl

--- dsa/src/main/java/commonsense/sort/InsertionSort.java
+++ dsa/src/main/java/commonsense/sort/InsertionSort.java
@@ -2,6 +2,21 @@ package commonsense.sort;
 
 public class InsertionSort {
     public int[] sort(int[] array) {
-        return  array;
+        for (int i = 0; i < array.length; i++) {
+            int idxToInsertValue = i;
+            int currValue = array[i];
+            for (int j = i - 1; j >= 0; j--) {
+                if(array[j] > currValue) {
+                    array[idxToInsertValue] = array[j];
+                    idxToInsertValue = j;
+                } else {
+                    break;
+                }
+            }
+            if (i != idxToInsertValue) {
+                array[idxToInsertValue] = currValue;
+            }
+        }
+        return array;
     }
 }
