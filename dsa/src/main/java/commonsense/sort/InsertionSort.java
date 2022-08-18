2a22aba00cd477f52b46c5e522098c5cd39b0b62
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-18 05:39:12 +0100

    Add InsertionSort impl

--- dsa/src/main/java/commonsense/sort/InsertionSort.java
+++ dsa/src/main/java/commonsense/sort/InsertionSort.java
@@ -39,7 +39,7 @@ public class InsertionSort {
                     break;
                 }
             }
-            array[position] = value;
+            array[position + 1] = value;
         }
         return array;
     }
