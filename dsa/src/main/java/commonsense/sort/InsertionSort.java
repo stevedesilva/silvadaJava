f4f5380a0088c27f14cb354aa923d4e036d38dad
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-18 04:55:17 +0100

    Add tests for InsertionSort

---
+++ dsa/src/main/java/commonsense/sort/InsertionSort.java
@@ -0,0 +1,7 @@
+package commonsense.sort;
+
+public class InsertionSort {
+    public int[] sort(int[] array) {
+        return  array;
+    }
+}
---
+++ dsa/src/test/java/commonsense/sort/InsertionSortTest.java
@@ -0,0 +1,18 @@
+package commonsense.sort;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+class InsertionSortTest {
+    @ParameterizedTest
+    @CsvSource(value = {"1:1", "2,1:1,2", "3,2,1:1,2,3", "4,3,2,1:1,2,3,4","5,4,3,2,1:1,2,3,4,5"}, delimiter = ':')
+    public void shouldSortArray(String input, String output) {
+        final int[] inputUnsortedInts= Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
+        final int[] expectedSortedInts = Arrays.stream(output.split(",")).mapToInt(Integer::parseInt).toArray();
+        MatcherAssert.assertThat(new InsertionSort().sort(inputUnsortedInts), Matchers.is(expectedSortedInts));
+    }
+}
