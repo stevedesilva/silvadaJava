a5f559ae3fda754346832c9dea6f97c134515c58
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-27 23:49:11 +0100

    Add mean number impl

--- dsa/src/main/java/commonsense/array/MeanNumbers.java
+++ dsa/src/main/java/commonsense/array/MeanNumbers.java
@@ -2,6 +2,19 @@ package commonsense.array;
 
 public class MeanNumbers {
     public static float getMeanFromEvenNumbers(int[] array) {
-        return 0.0f;
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]%2==0) {
+                count++;
+                sum += array[i];
+            }
+        }
+
+        if (count < 1) {
+            return 0;
+        }
+        return sum/count;
     }
 }
--- dsa/src/test/java/commonsense/array/MeanNumbersTest.java
+++ dsa/src/test/java/commonsense/array/MeanNumbersTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MeanNumbersTest {
     @ParameterizedTest
-    @CsvSource(value = {"1,2,3:2.0","2,2,2:2.0","12,12,6:10.0"})
+    @CsvSource(value = {"1,2,3:2.0","2,2,2:2.0","12,12,6:10.0"},delimiter = ':')
     public void shouldReturnMeanFromEvenNumbers(String input, float result) {
         final int[] ints = Arrays.stream(input.split(",")).mapToInt(t -> Integer.parseInt(t)).toArray();
         MatcherAssert.assertThat(MeanNumbers.getMeanFromEvenNumbers(ints), Matchers.equalTo(result));
