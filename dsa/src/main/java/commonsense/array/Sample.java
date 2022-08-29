7da7024dce1030abebef64c087c94680b11fb2a3
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-08-29 21:51:21 +0100

    Adding java version fo sampler

--- dsa/src/main/java/commonsense/array/Sample.java
+++ dsa/src/main/java/commonsense/array/Sample.java
@@ -1,6 +1,22 @@
 package commonsense.array;
 
+import java.util.Objects;
+
 public class Sample {
+    public Sample() {
+    }
+
+    public static Result getStartMiddleEndElements(int[] array) {
+        if (array.length <= 0) {
+            throw new IllegalArgumentException("need at least one value in array");
+        }
+        int start = array[0];
+        int middle = array[array.length / 2];
+        int end = array[array.length - 1];
+
+        return new Result(start, middle, end);
+    }
+
     public static class Result {
         int start;
         int middle;
@@ -11,16 +27,20 @@ public class Sample {
             this.middle = middle;
             this.end = end;
         }
-    }
 
-    public static Result arraySample(int[] array) {
-        if (array.length < 1) {
-            return new Result(0, 0, 0);
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Result result = (Result) o;
+            return start == result.start &&
+                    middle == result.middle &&
+                    end == result.end;
         }
-        int start = array[0];
-        int middle = array[array.length / 2];
-        int end = array[array.length - 1];
 
-        return new Result(start, middle, end);
+        @Override
+        public int hashCode() {
+            return Objects.hash(start, middle, end);
+        }
     }
 }
--- dsa/src/test/java/commonsense/array/SampleTest.java
+++ dsa/src/test/java/commonsense/array/SampleTest.java
@@ -1,8 +1,31 @@
 package commonsense.array;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
+
+
 
 class SampleTest {
-
-
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3:1,2,3","1:1,1,1"}, delimiter = ':')
+    public void shouldReturnSample(String input, String output) {
+        final int[] inputAsInts = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
+        final int[] outputAsInts = Arrays.stream(output.split(",")).mapToInt(Integer::parseInt).toArray();
+        final Sample.Result actual = Sample.getStartMiddleEndElements(inputAsInts);
+        Sample.Result result = new Sample.Result(outputAsInts[0],outputAsInts[1],outputAsInts[2]);
+        MatcherAssert.assertThat(actual, Matchers.equalTo(result));
+    }
+    @Test
+    public void shouldThrowExceptionWhenLessThanOneInputItem() {
+        final IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Sample.getStartMiddleEndElements(new int[]{});
+        });
+        MatcherAssert.assertThat(exception.getMessage(), Matchers.equalTo("need at least one value in array"));
+    }
 }
