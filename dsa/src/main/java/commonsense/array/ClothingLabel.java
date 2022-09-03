f13972b7c820f682b63a431e3f17d871e68fb6f1
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-09-03 09:35:10 +0100

    Add test for clothing label kata

---
+++ dsa/src/main/java/commonsense/array/ClothingLabel.java
@@ -0,0 +1,10 @@
+package commonsense.array;
+
+import java.util.List;
+
+public class ClothingLabel {
+    public String[] markInventory(String[] clothingItems) {
+        String[] result = new String[clothingItems.length];
+        return result;
+    }
+}
