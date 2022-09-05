c2fe1a908a3aaf23ab8cb4ae8265930498c8ed1a
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-09-05 07:18:00 +0100

    initial fix

--- dsa/src/main/java/commonsense/array/ClothingLabel.java
+++ dsa/src/main/java/commonsense/array/ClothingLabel.java
@@ -3,8 +3,15 @@ package commonsense.array;
 import java.util.List;
 
 public class ClothingLabel {
-    public String[] markInventory(String[] clothingItems) {
-        String[] result = new String[clothingItems.length];
+    public String[][] markInventory(String[] clothingItems) {
+        final int itemSizes = 5;
+        String[][] result = new String[clothingItems.length][itemSizes];
+        for(int i = 0; i< result.length; i++) {
+            for(int j = 0; j< itemSizes; j++) {
+                final String item = clothingItems[i] + " Size: " + (j + 1);
+                result[i][j] = item;
+            }
+        }
         return result;
     }
 }
--- dsa/src/test/java/commonsense/array/ClothingLabelTest.java
+++ dsa/src/test/java/commonsense/array/ClothingLabelTest.java
@@ -1,24 +1,21 @@
 package commonsense.array;
 
-import org.hamcrest.Matcher;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ClothingLabelTest {
     @ParameterizedTest
     @CsvSource(value = {
-            "Purple Shirt-Purple Shirt Size 1,Purple Shirt Size: 2,Purple Shirt Size: 3,Purple Shirt Size: 4,Purple Shirt Size: 5",
-            "Green Shirt-Green Shirt Size 1,Green Shirt Size: 2,Green Shirt Size: 3,Green Shirt Size: 4,Green Shirt Size: 5",
-            "Red Shirt-Red Shirt Size 1,Red Shirt Size: 2,Red Shirt Size: 3,Red Shirt Size: 4,Red Shirt Size: 5",
+            "Purple Shirt-Purple Shirt Size: 1,Purple Shirt Size: 2,Purple Shirt Size: 3,Purple Shirt Size: 4,Purple Shirt Size: 5",
+            "Green Shirt-Green Shirt Size: 1,Green Shirt Size: 2,Green Shirt Size: 3,Green Shirt Size: 4,Green Shirt Size: 5",
+            "Red Shirt-Red Shirt Size: 1,Red Shirt Size: 2,Red Shirt Size: 3,Red Shirt Size: 4,Red Shirt Size: 5",
     }, delimiter = '-')
     public void givenInventoryList_shouldReturn(String input, String output) {
-        String[] inParms = input.split(",");
-        String[] expected = output.split(",");
-        MatcherAssert.assertThat(new ClothingLabel().markInventory(inParms), Matchers.equalTo(expected));
+        final String[] splitOutput = output.split(",");
+        final String[][] actual = new ClothingLabel().markInventory(input.split(","));
+        MatcherAssert.assertThat(actual[0], Matchers.equalTo(splitOutput));
     }
 
 }
