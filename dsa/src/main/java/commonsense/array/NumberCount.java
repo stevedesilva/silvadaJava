39f63c27db23d0cdd5cd94627ccb070645b87749
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-09-11 09:53:38 +0100

    add implementation

--- dsa/src/main/java/commonsense/array/NumberCount.java
+++ dsa/src/main/java/commonsense/array/NumberCount.java
@@ -5,6 +5,14 @@ public class NumberCount {
         if (array == null || array.length <= 0) {
             throw new IllegalArgumentException("input array is mandatory");
         }
-        return 0;
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
--- dsa/src/test/java/commonsense/array/NumberCountTest.java
+++ dsa/src/test/java/commonsense/array/NumberCountTest.java
@@ -4,17 +4,34 @@ import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
-import java.util.regex.Matcher;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class NumberCountTest {
 
-//    @Test
-//    public void shouldThrowExceptionWhenInputArrayIsEmpty(){
-//        int expected = 1;
-//        int[][] input = new int[][]{};
-//    }
+    @Test
+    public void shouldReturnTotalOfOnesAlt(){
+        int expected = 5;
+        int[][] input = new int[][]{{0, 0, 0, 1}, {1, 1, 0}, {0, 0, 1}, {1}, {0}};
+        final int got = NumberCount.countTheOnes(input);
+        MatcherAssert.assertThat(got,Matchers.equalTo(expected));
+    }
+
+    @Test
+    public void shouldReturnTotalOfOnes(){
+        int expected = 6;
+        int[][] input = new int[][]{{0, 1, 0, 0, 1}, {1, 1, 0}, {0, 0, 1}, {1}, {0}};
+        final int got = NumberCount.countTheOnes(input);
+        MatcherAssert.assertThat(got,Matchers.equalTo(expected));
+    }
+
+    @Test
+    public void shouldReturnTotalOfOnesForEmptyInnerArray(){
+        int expected = 0;
+        int[][] input = new int[][]{{}, {}, {}, {}, {}};
+        final int got = NumberCount.countTheOnes(input);
+        MatcherAssert.assertThat(got,Matchers.equalTo(expected));
+    }
+
     @Test
     public void shouldThrowExceptionWhenInputArrayIsEmpty(){
         final IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
