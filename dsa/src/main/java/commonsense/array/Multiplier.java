9ceffbe2d8107753e7cede29ee3b6460ccbcfa01
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-09-15 13:27:27 +0100

    add error path

--- dsa/src/main/java/commonsense/array/Multiplier.java
+++ dsa/src/main/java/commonsense/array/Multiplier.java
@@ -6,6 +6,9 @@ import java.util.List;
 public class Multiplier {
 
     public static int[] findTheProduct(int[] array) {
+        if (array == null || array.length < 2) {
+            throw new IllegalArgumentException("a minimum input of 2 numbers is required");
+        }
         List<Integer> result = new ArrayList<>();
         for (int i=0; i < array.length; i++) {
             for (int j=i+1; j < array.length; j++) {
--- dsa/src/test/java/commonsense/array/MultiplierTest.java
+++ dsa/src/test/java/commonsense/array/MultiplierTest.java
@@ -2,6 +2,8 @@ package commonsense.array;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -9,12 +11,25 @@ import java.util.Arrays;
 
 class MultiplierTest {
     @ParameterizedTest
-    @CsvSource(value = {"1,2:2"}, delimiter = ':')
+    @CsvSource(value = {"1,:1", "1,2:2"}, delimiter = ':')
     public void shouldProduceMultiplesOfInputArray(String input, String output) {
         final int[] inputArray = getIntArrayFromString(input);
         final int[] result = Multiplier.findTheProduct(inputArray);
         final int[] expected = getIntArrayFromString(output);
         MatcherAssert.assertThat(result, Matchers.equalTo(expected));
+    }
+
+    @Test
+    public void throwsExceptionWhenMinimumInputNotMet() {
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Multiplier.findTheProduct(new int[]{1});
+        });
+        MatcherAssert.assertThat(exception.getMessage(), Matchers.equalTo("a minimum input of 2 numbers is required"));
+
+        exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Multiplier.findTheProduct(new int[]{});
+        });
+        MatcherAssert.assertThat(exception.getMessage(), Matchers.equalTo("a minimum input of 2 numbers is required"));
     }
 
     private int[] getIntArrayFromString(String input) {
