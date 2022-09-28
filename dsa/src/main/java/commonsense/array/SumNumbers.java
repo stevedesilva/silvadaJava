140e7358acad3034e84e84972d9c18dd03fde09f
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-09-29 00:56:51 +0100

    add implementation

--- dsa/src/main/java/commonsense/array/SumNumbers.java
+++ dsa/src/main/java/commonsense/array/SumNumbers.java
@@ -5,7 +5,12 @@ public class SumNumbers {
         if (array == null || array.length < 2) {
             throw new IllegalArgumentException("minimum of 2 values expected");
         }
-
+        for (int i = 0, j = array.length - 1; i < j; i++,j--) {
+            if (array[i] + array[j] == 100) {
+                return true;
+            }
+        }
         return false;
     }
 }
+
