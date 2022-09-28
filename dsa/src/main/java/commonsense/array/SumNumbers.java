cf3465b81bc2c19609fb88871491da0cf8967f18
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-09-29 00:43:12 +0100

    Adding tests for failure scenario

--- dsa/src/main/java/commonsense/array/SumNumbers.java
+++ dsa/src/main/java/commonsense/array/SumNumbers.java
@@ -2,6 +2,9 @@ package commonsense.array;
 
 public class SumNumbers {
     public static boolean outerAndInnerValuesEqualHundred(int[] array) {
+        if (array == null || array.length < 2) {
+            throw new IllegalArgumentException("minimum of 2 values expected");
+        }
         return false;
     }
 }
--- dsa/src/test/java/commonsense/array/SumNumbersTest.java
+++ dsa/src/test/java/commonsense/array/SumNumbersTest.java
@@ -1,27 +1,34 @@
 package commonsense.array;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.regex.Matcher;
+
 class SumNumbersTest {
     @Test
     public void shouldThrowExceptionWhenMinimumInputNotMetEmptyArray() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        final IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
             SumNumbers.outerAndInnerValuesEqualHundred(new int[]{});
         });
+        MatcherAssert.assertThat(exception.getMessage(), Matchers.equalTo("minimum of 2 values expected"));
     }
     @Test
     public void shouldThrowExceptionWhenMinimumInputNotMetOneValue() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        final IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
             SumNumbers.outerAndInnerValuesEqualHundred(new int[]{1});
         });
+        MatcherAssert.assertThat(exception.getMessage(), Matchers.equalTo("minimum of 2 values expected"));
     }
 
     @ParameterizedTest
     @CsvSource(value = {"10,10:false","50,50:true"}, delimiter = ':')
     public void innerAndOuterValuesWorkingInwardsShouldEqual100(String input, boolean output){
 
+        SumNumbers.outerAndInnerValuesEqualHundred(new int[]{1});
     }
 }
