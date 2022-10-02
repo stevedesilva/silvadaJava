d5683c4282031c9f65910b20e89c9555a55056f3
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-10-02 13:23:59 +0100

    Add implementation

--- dsa/src/main/java/commonsense/array/Merge.java
+++ dsa/src/main/java/commonsense/array/Merge.java
@@ -3,15 +3,25 @@ package commonsense.array;
 public class Merge {
 
     public int[] mergeArrays(int[] a1, int[] a2) {
-        if (a1.length < 1 && a2.length > 1) {
-            return a2;
-        }
-        if (a2.length < 1 && a1.length > 1) {
-            return a1;
-        }
 
         int[] result = new int[a1.length + a2.length];
-
+        int idx = 0, i = 0, j = 0;
+        while (i < a1.length || j < a2.length) {
+            if (i < a1.length && j >= a2.length) {
+                result[idx] = a1[i];
+                i++;
+            } else if (j < a2.length && i >= a1.length) {
+                result[idx] = a2[j];
+                j++;
+            } else if (a1[i] <= a2[j]) {
+                result[idx] = a1[i];
+                i++;
+            } else {
+                result[idx] = a2[j];
+                j++;
+            }
+            idx++;
+        }
         return result;
     }
 }
--- dsa/src/test/java/commonsense/array/MergeTest.java
+++ dsa/src/test/java/commonsense/array/MergeTest.java
@@ -26,28 +26,35 @@ class MergeTest {
     public void oneEmptyArray(String input, String output) {
         final int[] full = toIntArray(input);
         final int[] empty = {};
-        expectOneArray(output, full, empty);
-        expectOneArray(output, empty, full);
-    }
-
-    private void expectOneArray(String output, int[] inputA, int[] inputB) {
-        final int[] result = new Merge().mergeArrays(inputA, inputB);
-
-        final int[] expected = toIntArray(output);
-        ;
-        MatcherAssert.assertThat(result, Matchers.is(expected));
+        expectArray(output, full, empty);
+        expectArray(output, empty, full);
     }
 
     @ParameterizedTest
-    @CsvSource(value = {"1,2-3,4:1,2,3,4"}, delimiter = ':')
+    @CsvSource(value = {
+            "1-2:1,2",
+            "2-1:1,2",
+            "1,2-3,4:1,2,3,4",
+            "3,4-1,2:1,2,3,4",
+            "2,4-1,3:1,2,3,4",
+            "3,4-1,2:1,2,3,4",
+            "1,2-3,4,5,6:1,2,3,4,5,6",
+            "1,2,5,6-3,4:1,2,3,4,5,6",
+    }, delimiter = ':')
     public void shouldMergeTwoArrays(String input, String output) {
         final String[] inArray = input.split("-");
         final int[] inputA = toIntArray(inArray[0]);
         final int[] inputB = toIntArray(inArray[1]);
-        expectOneArray(output, inputA, inputB);
+        expectArray(output, inputA, inputB);
     }
 
     private int[] toIntArray(String output) {
         return Arrays.stream(output.split(",")).mapToInt(Integer::parseInt).toArray();
+    }
+
+    private void expectArray(String output, int[] inputA, int[] inputB) {
+        final int[] result = new Merge().mergeArrays(inputA, inputB);
+        final int[] expected = toIntArray(output);
+        MatcherAssert.assertThat(result, Matchers.is(expected));
     }
 }
