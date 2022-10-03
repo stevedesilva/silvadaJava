b94e61e286602fb6090c0a85d0298d7d8833cbce
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-10-03 11:29:24 +0100

    add test cases of needle in haystack kata

--- dsa/src/main/java/commonsense/array/NeedleInHaystack.java
+++ dsa/src/main/java/commonsense/array/NeedleInHaystack.java
@@ -1,20 +1,24 @@
 package commonsense.array;
 
-import java.nio.charset.Charset;
-
 public class NeedleInHaystack {
-    public static boolean findNeedle(String needle, String haystack) {
+    public static boolean findNeedle(String needle, String haystack) throws IllegalArgumentException {
         if (needle.isEmpty() || haystack.isEmpty()) {
             throw new IllegalArgumentException("needle and haystack input cannot be empty");
         }
+        boolean foundNeedle = false;
         for (int i = 0; i < haystack.length(); i++) {
             for (int  j = 0; j < needle.length(); j++) {
-
-//                if (needle[j]. != haystack[i]) {
-//
-//                }
+                foundNeedle = true;
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    foundNeedle = false;
+                    break;
+                }
+            }
+            if (foundNeedle) {
+                // return early since we have found our needle
+                break;
             }
         }
-        return false;
+        return foundNeedle;
     }
 }
--- dsa/src/test/java/commonsense/array/NeedleInHaystackTest.java
+++ dsa/src/test/java/commonsense/array/NeedleInHaystackTest.java
@@ -2,7 +2,6 @@ package commonsense.array;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -14,16 +13,15 @@ class NeedleInHaystackTest {
     @CsvSource(value = {
             "a,a:true",
             "a,b:false",
-            "acbbac,b:true",
+            "acbbac,bb:true",
             "acbfbac,fe:false",
             "thequickandthedead,and:true",
             "thequickandthedead,andd:false",
-
     }, delimiter = ':')
-    public void shouldFindNeedleInHaystack(String input, boolean expected) {
+    public void shouldFindNeedleInHaystack(String input, Boolean expected) {
         String[] array = input.split(",");
-        String needle = array[0];
-        String haystack = array[1];
+        String haystack = array[0];
+        String needle = array[1];
         MatcherAssert.assertThat(NeedleInHaystack.findNeedle(needle, haystack), Matchers.is(expected));
     }
 
@@ -32,7 +30,6 @@ class NeedleInHaystackTest {
         final IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
             NeedleInHaystack.findNeedle("", "");
         });
-
         MatcherAssert.assertThat(exception.getMessage(), Matchers.is("needle and haystack input cannot be empty"));
     }
 }
