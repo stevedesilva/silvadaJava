fb9e1d71bbf386f9baf1dc3381bbbc3cffd0da16
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-10-07 08:12:36 +0100

    Add impl for largest product

--- dsa/src/main/java/commonsense/array/LargestProduct.java
+++ dsa/src/main/java/commonsense/array/LargestProduct.java
@@ -1,7 +1,23 @@
 package commonsense.array;
 
 public class LargestProduct {
+    public static final String MINIMUM_OF_3_VALUES_REQUIRED = "minimum of 3 values required";
+
     public static int findLargestProduct(int[] array) throws IllegalArgumentException {
-        return 0;
+        if (array == null || array.length < 3) {
+            throw new IllegalArgumentException(MINIMUM_OF_3_VALUES_REQUIRED);
+        }
+        int largestProduct = 0;
+        for(int i = 0; i < array.length; i++) {
+            for(int j = i + 1; j < array.length; j++) {
+                for(int k = j + 1; k < array.length; k++) {
+                    final int product = array[i] * array[j] * array[k];
+                    if(largestProduct < product) {
+                        largestProduct = product;
+                    }
+                }
+            }
+        }
+        return largestProduct;
     }
 }
--- dsa/src/test/java/commonsense/array/LargestProductTest.java
+++ dsa/src/test/java/commonsense/array/LargestProductTest.java
@@ -2,17 +2,26 @@ package commonsense.array;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 
 class LargestProductTest {
+    @Test
+    public void shouldThrowExceptionWhen() {
+        final IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () ->
+                LargestProduct.findLargestProduct(new int[]{})
+        );
+        MatcherAssert.assertThat(exception.getMessage(), Matchers.equalTo(LargestProduct.MINIMUM_OF_3_VALUES_REQUIRED));
+    }
+
     @ParameterizedTest
-    @CsvSource(value = {"1,2,3:6","1,2,3,4:24",}, delimiter = ':')
+    @CsvSource(value = {"1,2,3:6","1,2,3,4:24","5,5,3,4,5:125","1,2,3,4,5:60"}, delimiter = ':')
     public void shouldReturnLargestProduct(String input, int expected) {
         final int[] array = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
         MatcherAssert.assertThat(LargestProduct.findLargestProduct(array), Matchers.equalTo(expected));
     }
-
 }
