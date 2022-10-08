26a86f89619fb3b631fa170f60e6fc04bdd89e12
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-10-08 02:24:54 +0100

    Refactor impl for Luhn algorithm

--- dsa/src/test/java/commonsense/array/LuhnAlgorithmTest.java
+++ dsa/src/test/java/commonsense/array/LuhnAlgorithmTest.java
@@ -5,10 +5,6 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import java.util.Arrays;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class LuhnAlgorithmTest {
     @ParameterizedTest
     @CsvSource(value = {"49927398716,true", "49927398717,false","1234567812345678,false","1234567812345670,true","11111666,false"})
