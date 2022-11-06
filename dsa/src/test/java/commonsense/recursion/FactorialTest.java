fa7c0203934f08f88961ec76bfe8346ecda24a95
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-11-06 16:45:08 +0000

    cleanup

--- dsa/src/test/java/commonsense/recursion/FactorialTest.java
+++ dsa/src/test/java/commonsense/recursion/FactorialTest.java
@@ -5,8 +5,6 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FactorialTest {
     @ParameterizedTest
     @CsvSource( value = {"1,1","2,2","3,6","4,24","5,120"})
@@ -14,5 +12,4 @@ class FactorialTest {
         Factorial factorial = new Factorial();
         MatcherAssert.assertThat(factorial.calculateFactorial(input), Matchers.equalTo(expected));
     }
-
 }
