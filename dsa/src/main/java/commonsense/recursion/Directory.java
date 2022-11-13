9f1fb764047007967ed07a9370e24acd3f50a257
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-11-13 20:32:03 +0000

    Add test directories

---
+++ dsa/src/main/java/commonsense/recursion/Directory.java
@@ -0,0 +1,11 @@
+package commonsense.recursion;
+
+import java.util.List;
+
+public class Directory {
+    public static List<String> findDirectories(String directory) {
+        return null;
+    }
+
+
+}
---
+++ dsa/src/test/java/commonsense/recursion/DirectoryTest.java
@@ -0,0 +1,74 @@
+package commonsense.recursion;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class DirectoryTest {
+
+    @Test
+    /*
+      Temp locations are found here: System.getProperty("java.io.tmpdir")
+     */
+    void findDirectories() {
+        // Given a test directory structure with files
+        List<String> expected = new ArrayList<>();
+        try {
+            final Path level_root = createDirectoryAndFiles("level_root_","file1","file2");
+            expected.add(level_root.toFile().getAbsolutePath());
+            level_root.toFile().deleteOnExit();
+
+            final Path level1 = createDirectoryAndFiles(level_root, "level_1_", "file3", "file4");
+            expected.add(level1.toFile().getAbsolutePath());
+
+            final Path level2 = createDirectoryAndFiles(level1, "level_2_", "file4", "file5");
+            expected.add(level2.toFile().getAbsolutePath());
+
+            final Path level3 = createDirectoryAndFiles(level2, "level_3_", "file6", "file7");
+            expected.add(level3.toFile().getAbsolutePath());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // When called
+        String root = "";
+        final List<String> directories = Directory.findDirectories(root);
+
+        // Then only directories are returned
+        MatcherAssert.assertThat(directories, Matchers.equalTo(expected));
+    }
+
+    private Path createDirectoryAndFiles(String prefix, String... files) throws IOException {
+        Path level = Files.createTempDirectory(prefix);
+        Arrays.stream(files).forEach((f) -> {
+            try {
+                 new File(level.toAbsolutePath() + "/" + f + ".txt").createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return level.toAbsolutePath();
+    }
+    private Path createDirectoryAndFiles(Path path, String prefix, String... files) throws IOException {
+        final Path dir = path.toAbsolutePath();
+        Path level = Files.createTempDirectory(dir, prefix);
+        Arrays.stream(files).forEach((f) -> {
+            try {
+                 new File(level.toAbsolutePath() + "/" + f + ".txt").createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return level.toAbsolutePath();
+    }
+}
+
