628e0af3107995fe776c9d112d564d97d7141525
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-11-14 20:33:19 +0000

    refactoring

--- dsa/src/test/java/commonsense/recursion/DirectoryTest.java
+++ dsa/src/test/java/commonsense/recursion/DirectoryTest.java
@@ -14,17 +14,26 @@ import java.util.Arrays;
 import java.util.List;
 
 class DirectoryTest {
-
     @Test
-    /*
-      Temp locations are found here: System.getProperty("java.io.tmpdir")
-     */
-    void findDirectories() {
+    void shouldFindDirectories() {
+        // Temp locations are found here: System.getProperty("java.io.tmpdir")
+
         // Given a test directory structure with files
+        List<String> expected = createTestDirectoriesWithFiles();
+
+        // When called
+        String root = "";
+        final List<String> directories = Directory.findDirectories(root);
+
+        // Then only directories are returned
+        MatcherAssert.assertThat(directories, Matchers.equalTo(expected));
+    }
+
+    private List<String> createTestDirectoriesWithFiles() {
         List<String> expected = new ArrayList<>();
         final Path level_root;
         try {
-            level_root = createDirectoryAndFiles("level_root_","file1","file2");
+            level_root = createDirectoryAndFiles("level_root_", "file1", "file2");
             expected.add(level_root.toFile().getAbsolutePath());
 
             final Path level1 = createDirectoryAndFiles(level_root, "level_1_", "file3", "file4");
@@ -40,7 +49,7 @@ class DirectoryTest {
             throw new RuntimeException(e);
         }
 
-        try(DirectoryStream<Path> ds = Files.newDirectoryStream(level_root)) {
+        try (DirectoryStream<Path> ds = Files.newDirectoryStream(level_root)) {
             level_root.toFile().deleteOnExit();
 
             for (Path file : ds) {
@@ -49,33 +58,28 @@ class DirectoryTest {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
-        // When called
-        String root = "";
-        final List<String> directories = Directory.findDirectories(root);
-
-        // Then only directories are returned
-        MatcherAssert.assertThat(directories, Matchers.equalTo(expected));
+        return expected;
     }
+
 
     private Path createDirectoryAndFiles(String prefix, String... files) throws IOException {
         Path level = Files.createTempDirectory(prefix);
         Arrays.stream(files).forEach((f) -> {
             try {
-                 new File(level.toAbsolutePath() + "/" + f + ".txt").createNewFile();
+                new File(level.toAbsolutePath() + "/" + f + ".txt").createNewFile();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         });
         return level.toAbsolutePath();
     }
+
     private Path createDirectoryAndFiles(Path path, String prefix, String... files) throws IOException {
         final Path dir = path.toAbsolutePath();
         Path level = Files.createTempDirectory(dir, prefix);
         Arrays.stream(files).forEach((f) -> {
             try {
-                 new File(level.toAbsolutePath() + "/" + f + ".txt").createNewFile();
+                new File(level.toAbsolutePath() + "/" + f + ".txt").createNewFile();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
