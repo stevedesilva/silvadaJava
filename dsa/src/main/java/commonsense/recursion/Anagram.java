a46515fdbda1324119311f4102b53fc160ef1ae8
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-12-20 19:19:07 +0000

    add anagram impl

--- dsa/src/main/java/commonsense/recursion/Anagram.java
+++ dsa/src/main/java/commonsense/recursion/Anagram.java
@@ -6,8 +6,29 @@ import java.util.List;
 public class Anagram {
 
     public static List<String> findAllPossibleAnagrams(String word) {
-        List<String> result = new ArrayList<>();
-        return result;
+        List<String> allPossibleAnagrams = new ArrayList<>();
+        // base case
+        if (word == null || word.length() <= 1) {
+            // base case is a single char string
+             allPossibleAnagrams.add(word);
+             return allPossibleAnagrams;
+        }
+        // find sub anagrams
+        Character firstChar = word.charAt(0);
+        String subString = word.substring(1);
+        List<String> subAnagrams = findAllPossibleAnagrams(subString);
+
+        // for each anagram find all possible combos
+        subAnagrams.forEach( anagram -> {
+            for (int i = 0; i <= anagram.length(); i++) {
+                var before = anagram.substring(0,i) ;
+                var after = anagram.substring(i);
+                StringBuilder sb = new StringBuilder();
+                sb.append(before).append(firstChar).append(after);
+                allPossibleAnagrams.add(sb.toString());
+            }
+        });
+        return allPossibleAnagrams;
     }
 
 }
--- dsa/src/test/java/commonsense/recursion/AnagramTest.java
+++ dsa/src/test/java/commonsense/recursion/AnagramTest.java
@@ -7,13 +7,19 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class AnagramTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"a:a","ab:ab,ba","abc:abc,bac,acb,acb,cab,cba"}, delimiter = ':')
+    @CsvSource(value = {
+            "a:a",
+            "ab:ab,ba",
+            "abc:abc,bac,bca,acb,cab,cba",
+            "abcd:abcd, bacd, bcad, bcda, acbd, cabd, cbad, cbda, acdb, cadb, cdab, cdba, abdc, badc, bdac, bdca, adbc, dabc, dbac, dbca, adcb, dacb, dcab, dcba",
+    }, delimiter = ':')
     public void shouldFindAllPossibleVariations(String word, String expected){
-        final List<String> expectedAnagrams = Arrays.asList(expected.split(","));
+        final List<String> expectedAnagrams = Arrays.stream(expected.split(",")).map(String::trim).collect(Collectors.toList());
         final List<String> result = Anagram.findAllPossibleAnagrams(word);
         MatcherAssert.assertThat(result,Matchers.equalTo(expectedAnagrams));
     }
