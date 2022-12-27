f193eccab665c1d72f2487c271c504c73d2f225b
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-12-27 14:47:50 +0000

    adding test for triangular numbers

---
+++ dsa/src/main/java/commonsense/recursion/TriangularNumbers.java
@@ -0,0 +1,13 @@
+package commonsense.recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TriangularNumbers {
+
+    public static int execute(int number) {
+        return 0;
+    }
+
+}
---
+++ dsa/src/test/java/commonsense/recursion/TriangularNumbersTest.java
@@ -0,0 +1,16 @@
+package commonsense.recursion;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TriangularNumbersTest {
+    @ParameterizedTest
+    @CsvSource(value = {"1,1","2,3","3,6","4,10","5,15","6,21","7,28","8,36","9,45","10,55"})
+    public void shouldReturnTriangularNumbers(int input, int expected) {
+        MatcherAssert.assertThat(TriangularNumbers.execute(input), Matchers.equalTo(expected));
+    }
+}
