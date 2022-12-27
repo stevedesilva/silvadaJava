df44953ba63c583d28e441b6f981bd56bd6af307
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-12-27 05:40:08 +0000

    adding impl for count even numbers

--- dsa/src/main/java/commonsense/recursion/CountEvenNumbers.java
+++ dsa/src/main/java/commonsense/recursion/CountEvenNumbers.java
@@ -1,10 +1,22 @@
 package commonsense.recursion;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CountEvenNumbers {
 
     public static List<Integer> countEvenNumbers(int[] array) {
-        return null;
+        return countEvenNumbersRec(array, new ArrayList<>());
+    }
+
+    public static List<Integer> countEvenNumbersRec(int[] array, List<Integer> result) {
+        if(array == null || array.length == 0){
+            return result;
+        }
+        if (array[0] % 2 == 0) {
+            result.add(array[0]);
+        }
+        return countEvenNumbersRec(Arrays.copyOfRange(array, 1, array.length), result);
     }
 }
--- dsa/src/test/java/commonsense/recursion/CountEvenNumbersTest.java
+++ dsa/src/test/java/commonsense/recursion/CountEvenNumbersTest.java
@@ -5,6 +5,7 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,11 +15,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class CountEvenNumbersTest {
     @ParameterizedTest
     @CsvSource(delimiter = ':', value = {
-//           "1:",
-//           "2:2",
+            "1:",
+            "1,3,5,7,9:",
+            "2:2",
+            "2,4,6,8,10:2,4,6,8,10",
+            "1,2,3,4,5,6,7,8,9,10:2,4,6,8,10",
     })
     public void shouldReturnEvenNumbersFromArray(String input, String expected) {
-        final List<Integer> want = Arrays.stream(expected.split(",")).map(Integer::valueOf).collect(Collectors.toList());
+        List<Integer> want;
+        if (expected == null) {
+            want = new ArrayList<>();
+            ;
+        } else {
+            want = Arrays.stream(expected.split(",")).map(Integer::valueOf).collect(Collectors.toList());
+        }
         List<Integer> got = CountEvenNumbers.countEvenNumbers(Arrays.stream(input.split(",")).mapToInt(Integer::valueOf).toArray());
         MatcherAssert.assertThat(got, Matchers.equalTo(want));
     }
