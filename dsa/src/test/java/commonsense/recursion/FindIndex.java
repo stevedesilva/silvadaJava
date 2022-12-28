37f2069a8585462795515236a7753f826c0e47a5
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-12-28 18:24:31 +0000

    Add impl for char not found in string

--- dsa/src/test/java/commonsense/recursion/FindIndex.java
+++ dsa/src/test/java/commonsense/recursion/FindIndex.java
@@ -1,7 +1,18 @@
 package commonsense.recursion;
 
 public class FindIndex {
-    public static int findFirstIndex(String word, Character toFind ) throws IllegalArgumentException {
-        throw new IllegalArgumentException("Not found");
+    public static int findFirstIndex(String word, char toFind) throws IllegalArgumentException {
+        return findFirstIndexRec(word, toFind, 0);
     }
+
+    public static int findFirstIndexRec(String word, char toFind, int index) {
+        if (word.length() == index) {
+            throw new IllegalArgumentException("Not found");
+        }
+        if (word.charAt(index) == toFind) {
+            return index;
+        }
+        return findFirstIndexRec(word, toFind, index + 1);
+    }
+
 }
--- dsa/src/test/java/commonsense/recursion/FindIndexTest.java
+++ dsa/src/test/java/commonsense/recursion/FindIndexTest.java
@@ -1,21 +1,33 @@
 package commonsense.recursion;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class FindIndexTest {
-   @Test
+    @Test
     public void shouldThrowExceptionIfCharNotFoundInString() {
-       Assertions.assertThrows(IllegalArgumentException.class, () -> {
-           FindIndex.findFirstIndex("ssss",'c');
-       });
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            FindIndex.findFirstIndex("ssss", 'c');
+        });
     }
 
     @ParameterizedTest
-    @CsvSource("a,1")
-    public void shouldFindFirstIndexForXInString(String input, int index) {
-
+    @CsvSource(delimiter = ':', value = {
+            "x,x:0",
+            "ax,x:1",
+            "abcde,e:4",
+            "abcde,b:1",
+            "abcdefghijk,k:10",
+            "abcde,c:2",
+    })
+    public void shouldFindFirstIndexForXInString(String input, int expectedIndex) {
+        String[] inputs = input.split(",");
+        String word = inputs[0];
+        char charToFind = inputs[1].charAt(0);
+        MatcherAssert.assertThat(FindIndex.findFirstIndex(word, charToFind), Matchers.equalTo(expectedIndex));
     }
 }
