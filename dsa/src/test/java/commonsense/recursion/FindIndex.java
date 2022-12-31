028cac7c3bc921aee118aa2676b0acf87f4e727d
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2022-12-31 00:44:44 +0000

    Add impl alt for char not found in string

--- dsa/src/test/java/commonsense/recursion/FindIndex.java
+++ dsa/src/test/java/commonsense/recursion/FindIndex.java
@@ -1,5 +1,7 @@
 package commonsense.recursion;
 
+import java.util.Arrays;
+
 public class FindIndex {
     public static int findFirstIndex(String word, char toFind) throws IllegalArgumentException {
         return findFirstIndexRec(word, toFind, 0);
@@ -15,4 +17,17 @@ public class FindIndex {
         return findFirstIndexRec(word, toFind, index + 1);
     }
 
+    public static int findFirstIndexAlt(String word, char toFind) throws IllegalArgumentException {
+        return findFirstIndexRecAlt(word.toCharArray(), toFind);
+    }
+
+    public static int findFirstIndexRecAlt(char[] word, char toFind) {
+        if (word.length == 0) {
+            throw new IllegalArgumentException("Not found");
+        }
+        if (word[0] == toFind) {
+            return 0;
+        }
+        return findFirstIndexRecAlt(Arrays.copyOfRange(word,1,word.length), toFind) + 1;
+    }
 }
--- dsa/src/test/java/commonsense/recursion/FindIndexTest.java
+++ dsa/src/test/java/commonsense/recursion/FindIndexTest.java
@@ -30,4 +30,20 @@ class FindIndexTest {
         char charToFind = inputs[1].charAt(0);
         MatcherAssert.assertThat(FindIndex.findFirstIndex(word, charToFind), Matchers.equalTo(expectedIndex));
     }
+
+    @ParameterizedTest
+    @CsvSource(delimiter = ':', value = {
+            "x,x:0",
+            "ax,x:1",
+            "abcde,e:4",
+            "abcde,b:1",
+            "abcdefghijk,k:10",
+            "abcde,c:2",
+    })
+    public void shouldFindFirstIndexForXInStringAlt(String input, int expectedIndex) {
+        String[] inputs = input.split(",");
+        String word = inputs[0];
+        char charToFind = inputs[1].charAt(0);
+        MatcherAssert.assertThat(FindIndex.findFirstIndex(word, charToFind), Matchers.equalTo(expectedIndex));
+    }
 }
