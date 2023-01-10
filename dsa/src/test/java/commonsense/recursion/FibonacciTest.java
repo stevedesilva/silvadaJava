ff0abcbf724e6ee0923162981d23aa512d242c85
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-01-10 22:23:50 +0000

    Adding init for fibonacci

--- dsa/src/test/java/commonsense/recursion/FibonacciTest.java
+++ dsa/src/test/java/commonsense/recursion/FibonacciTest.java
@@ -9,8 +9,9 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FibonacciTest {
+    // 0 1 1 2 3 5 8 13 21 34 55
     @ParameterizedTest
-    @CsvSource(value = {"1,1"})
+    @CsvSource(value = {"0,0","1,1","2,1","3,3","4,5","5,8","6,13","7,21","8,34","9,55","10,89"})
     public void shouldGenerateFibonacciSequence(int input, int expected) {
         MatcherAssert.assertThat(Fibonacci.execute(input), Matchers.equalTo(expected));
     }
