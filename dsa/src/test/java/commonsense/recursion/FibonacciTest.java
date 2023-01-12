24971a3d62905a159b80a9ec15823fc2e2071a13
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-01-12 23:55:14 +0000

    Adding  fibonacci

--- dsa/src/test/java/commonsense/recursion/FibonacciTest.java
+++ dsa/src/test/java/commonsense/recursion/FibonacciTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class FibonacciTest {
     // 0 1 1 2 3 5 8 13 21 34 55
     @ParameterizedTest
-    @CsvSource(value = {"0,0","1,1","2,1","3,2","4,3","5,5","6,8","7,13","8,21","9,34","10,55"})
+    @CsvSource(value = {"0,0","1,1","2,1","3,2","4,3","5,5","6,8","7,13","8,21","9,34","10,55", "11,89"})
     public void shouldGenerateFibonacciSequence(int input, int expected) {
         MatcherAssert.assertThat(Fibonacci.fibonacci(input), Matchers.equalTo(expected));
     }
