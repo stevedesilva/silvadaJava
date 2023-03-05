d9df5c4166117f472855cb2d4a474fe237371c5e
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-03-05 16:39:09 +0000

    Add duplicate finder case

--- dsa/src/main/java/commonsense/sort/DuplicateFinder.java
+++ dsa/src/main/java/commonsense/sort/DuplicateFinder.java
@@ -1,9 +1,21 @@
 package commonsense.sort;
 
+import static commonsense.sort.QuickSelect.ARRAY_MUST_CONTAIN_AT_LEAST_ONE_VALUE;
+
 public class DuplicateFinder {
 
-
     public boolean hasDuplicates(int[] array) {
+        if (array.length < 1) {
+            throw new IllegalArgumentException(ARRAY_MUST_CONTAIN_AT_LEAST_ONE_VALUE);
+        }
+
+        final int[] sorted = new QuickSort(array).sort(0, array.length-1);
+        for (int i = 0; i < array.length - 1; i++){
+           if(sorted[i] == sorted[i + 1]) {
+               return true;
+           }
+        }
         return false;
     }
+
 }
