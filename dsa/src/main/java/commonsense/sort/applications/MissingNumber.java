37305d3394b8792b8963928dfd949db5931a0bd2
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-03-10 22:51:26 +0000

    fix tests

--- dsa/src/main/java/commonsense/sort/applications/MissingNumber.java
+++ dsa/src/main/java/commonsense/sort/applications/MissingNumber.java
@@ -5,17 +5,20 @@ import commonsense.sort.QuickSort;
 import java.util.NoSuchElementException;
 
 import static commonsense.sort.QuickSelect.ARRAY_MUST_CONTAIN_AT_LEAST_ONE_VALUE;
-
 public class MissingNumber {
+
+    public static final String NO_MISSING_ELEMENTS = "no missing elements";
+
     public static int findMissingNumber(int[] numbersToSearch) throws NoSuchElementException, IllegalArgumentException {
         if (numbersToSearch == null || numbersToSearch.length < 1) {
             throw new IllegalArgumentException(ARRAY_MUST_CONTAIN_AT_LEAST_ONE_VALUE);
         }
         final int[] sortedArray = new QuickSort(numbersToSearch).sort(0, numbersToSearch.length - 1);
         for (int i = 0; i < numbersToSearch.length; i++) {
-            if(numbersToSearch[i] != i){
-
+            if (numbersToSearch[i] != i) {
+                return i;
             }
         }
+        throw new NoSuchElementException(NO_MISSING_ELEMENTS);
     }
 }
--- dsa/src/test/java/commonsense/sort/applications/MissingNumberTest.java
+++ dsa/src/test/java/commonsense/sort/applications/MissingNumberTest.java
@@ -13,16 +13,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class MissingNumberTest {
     @ParameterizedTest
     @CsvSource(value = {
-            "1,2:1",
-            "1,2:3",
-            "1,2,3,5:4",
-            "1,2,3,4,6,7,8,9,10:5",
+            "0,2:1",
+            "5,2,4,1,0:3",
+            "0,1,2,3,5:4",
+            "0,1,2,3,4,6,7,8,9,10:5",
 
     },
             delimiter = ':')
     public void shouldFindMissingNumberFromArray(String input, int expected) {
-
-
         final int[] numsToSearch = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
 
         MatcherAssert.assertThat(MissingNumber.findMissingNumber(numsToSearch), Matchers.equalTo(expected));
