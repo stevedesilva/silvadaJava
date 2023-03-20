160dd9a912c4988862396366e4dcd2fa0ebf6caa
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-03-20 18:52:47 +0000

    Add node

---
+++ dsa/src/test/java/commonsense/linkedlist/Node.java
@@ -0,0 +1,15 @@
+package commonsense.linkedlist;
+
+public class Node<T> {
+    T value;
+    Node<T> next;
+
+    public Node(T value) {
+        this.value = value;
+    }
+
+    public Node(T value, Node<T> next) {
+        this.value = value;
+        this.next = next;
+    }
+}
