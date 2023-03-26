018cc5eb130ace5bf2500a17834d09905f9bb858
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-03-26 16:20:31 +0100

    Add test for reading a linked list

--- dsa/src/main/java/commonsense/linkedlist/LinkedList.java
+++ dsa/src/main/java/commonsense/linkedlist/LinkedList.java
@@ -2,6 +2,8 @@ package commonsense.linkedlist;
 
 import commonsense.node.Node;
 
+import java.util.NoSuchElementException;
+
 public class LinkedList<T> {
     Node<T> head;
 
@@ -9,8 +11,32 @@ public class LinkedList<T> {
         this.head = head;
     }
 
-    public T read(int index) {
+    public T read(int index) throws NoSuchElementException {
+        Node<T> currentNode = head;
+        int currentIndex = 0;
 
-        return null;
+        while (currentIndex < index) {
+            currentNode = currentNode.next;
+            currentIndex++;
+            if (currentNode == null) {
+                // end of list
+                throw new NoSuchElementException();
+            }
+        }
+        return currentNode.value;
     }
+
+    public int search(T value) {
+        Node<T> currentNode = head;
+        int currentIndex = 0;
+        while(currentNode != null) {
+            if (currentNode.value.equals(value)) {
+                return currentIndex;
+            }
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+        throw new NoSuchElementException();
+    }
+
 }
--- dsa/src/test/java/commonsense/linkedlist/LinkedListTest.java
+++ dsa/src/test/java/commonsense/linkedlist/LinkedListTest.java
@@ -3,7 +3,10 @@ package commonsense.linkedlist;
 import commonsense.node.Node;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
 
 class LinkedListTest {
 
@@ -23,17 +26,69 @@ class LinkedListTest {
 
     @Test
     public void shouldReadIndexOfLinkedList(){
+        Node<Integer> node4 = new Node<>(4);
+        Node<Integer> node3 = new Node<>(3);
+        Node<Integer> node2 = new Node<>(2);
+        Node<Integer> node1 = new Node<>(1);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        LinkedList<Integer> linkedList = new LinkedList<>(node1);
+
+        MatcherAssert.assertThat(linkedList.read(0), Matchers.equalTo(node1.value));
+        MatcherAssert.assertThat(linkedList.read(1), Matchers.equalTo(node2.value));
+        MatcherAssert.assertThat(linkedList.read(2), Matchers.equalTo(node3.value));
+        MatcherAssert.assertThat(linkedList.read(3), Matchers.equalTo(node4.value));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenReadIndexNotFoundInLinkedList(){
+        Node<Integer> node4 = new Node<>(4);
         Node<Integer> node3 = new Node<>(3);
         Node<Integer> node2 = new Node<>(2);
         Node<Integer> node1 = new Node<>(1);
         node1.next = node2;
         node2.next = node3;
-        LinkedList<Integer> linkedList = new LinkedList<>(node1);
+        node3.next = node4;
 
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
+            new LinkedList<>(node1).read(10);
+        });
 
-        MatcherAssert.assertThat(linkedList.read(0), Matchers.equalTo(node1.value));
-        MatcherAssert.assertThat(linkedList.read(1), Matchers.equalTo(node2.value));
-        MatcherAssert.assertThat(linkedList.read(2), Matchers.equalTo(node3.value));
     }
+
+    @Test
+    public void shouldSearchForValueInLinkedList(){
+        Node<String> node4 = new Node<>("d");
+        Node<String> node3 = new Node<>("c");
+        Node<String> node2 = new Node<>("b");
+        Node<String> node1 = new Node<>("a");
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        MatcherAssert.assertThat(new LinkedList<>(node1).search("c"), Matchers.equalTo(2));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSearchValueNotFoundInLinkedList(){
+        Node<String> node4 = new Node<>("d");
+        Node<String> node3 = new Node<>("c");
+        Node<String> node2 = new Node<>("b");
+        Node<String> node1 = new Node<>("a");
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
+            new LinkedList<>(node1).search("e");
+        });
+    }
+
+
+
+
+
 
 }
