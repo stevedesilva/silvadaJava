7bcec9aabc71f19cfeae727d9691d7a6637e1ea0
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-05-08 18:48:46 +0100

    Add skeleton test for DoublyLinkedList

--- dsa/src/main/java/commonsense/linkedlist/doubly/DoublyLinkedList.java
+++ dsa/src/main/java/commonsense/linkedlist/doubly/DoublyLinkedList.java
@@ -1,9 +1,102 @@
 package commonsense.linkedlist.doubly;
 
-public class DoublyLinkedList<T> {
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Predicate;
+
+public class DoublyLinkedList<T> implements AllowedLinkedListMethod<T>{
     private int size;
     private Node<T> head;
     private Node<T> tail;
 
 
+    @Override
+    public void addAtEnd(T value) {
+
+    }
+
+    @Override
+    public void addByIndex(int index, T value) throws NoSuchElementException {
+
+    }
+
+    @Override
+    public void addToFront(T value) {
+
+    }
+
+    @Override
+    public void deleteByIndex(int index) throws NoSuchElementException {
+
+    }
+
+    @Override
+    public void deleteFromEnd() throws NoSuchElementException {
+
+    }
+
+    @Override
+    public void deleteFromFront() throws NoSuchElementException {
+
+    }
+
+    @Override
+    public void deleteItems(Predicate<T> fn) {
+
+    }
+
+    @Override
+    public List<T> printItems() {
+        return null;
+    }
+
+    @Override
+    public T readByIndex(int index) throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public T readFromEnd() throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public T readFromFront() throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public T removeByIndex(int index) throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public T removeFromEnd() throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public T removeFromFront() throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public T search(T value) throws NoSuchElementException {
+        return null;
+    }
+
+    @Override
+    public int size(T value) {
+        return 0;
+    }
+
+    @Override
+    public commonsense.linkedlist.classic.Node<T> head() {
+        return null;
+    }
+
+    @Override
+    public commonsense.linkedlist.classic.Node<T> tail() {
+        return null;
+    }
 }
---
+++ dsa/src/test/java/commonsense/linkedlist/doubly/DoublyLinkedListTest.java
@@ -0,0 +1,97 @@
+package commonsense.linkedlist.doubly;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoublyLinkedListTest {
+    /*
+    func TestDoublyLinkedList_AddAtEnd(t *testing.T) {
+	list := NewDoublyLinkedList[int]()
+	list.AddAtEnd(1)
+	assert.Equal(t, 1, list.Size())
+	assert.Equal(t, 1, list.Head().data)
+	assert.Equal(t, 1, list.Tail().data)
+
+	list.AddAtEnd(2)
+	assert.Equal(t, 2, list.Size())
+	assert.Equal(t, 1, list.Head().data)
+	assert.Equal(t, 2, list.Tail().data)
+
+	assert.Equal(t, 2, list.Head().next.data)
+}
+     */
+
+    @Test
+    void addAtEnd() {
+
+    }
+
+    @Test
+    void addByIndex() {
+    }
+
+    @Test
+    void addToFront() {
+    }
+
+    @Test
+    void deleteByIndex() {
+    }
+
+    @Test
+    void deleteFromEnd() {
+    }
+
+    @Test
+    void deleteFromFront() {
+    }
+
+    @Test
+    void deleteItems() {
+    }
+
+    @Test
+    void printItems() {
+    }
+
+    @Test
+    void readByIndex() {
+    }
+
+    @Test
+    void readFromEnd() {
+    }
+
+    @Test
+    void readFromFront() {
+    }
+
+    @Test
+    void removeByIndex() {
+    }
+
+    @Test
+    void removeFromEnd() {
+    }
+
+    @Test
+    void removeFromFront() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void head() {
+    }
+
+    @Test
+    void tail() {
+    }
+}
