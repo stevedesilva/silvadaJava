428053423c2e7e86288ab575dc1ac203b5c5cd50
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2023-06-16 09:22:38 +0100

    Add comparator

--- dsa/src/main/java/commonsense/tree/binarysearch/TreeNode.java
+++ dsa/src/main/java/commonsense/tree/binarysearch/TreeNode.java
@@ -1,6 +1,10 @@
 package commonsense.tree.binarysearch;
 
-public class TreeNode<T> {
+import org.hamcrest.Matchers;
+
+import java.util.Comparator;
+
+public class TreeNode<T extends Comparable<T>> implements Comparator<T> {
     T value;
     TreeNode<T> left;
     TreeNode<T> right;
@@ -8,6 +12,7 @@ public class TreeNode<T> {
     public TreeNode(T value) {
         this.value = value;
     }
+
     public TreeNode(T value, TreeNode<T> left, TreeNode<T> right) {
         this.value = value;
         this.left = left;
@@ -17,7 +22,17 @@ public class TreeNode<T> {
     public TreeNode<T> Search(T value, TreeNode<T> node) {
         if (node.value == value) {
             return node;
+        } else if (compare(value, node.value) < 1) {
+            return Search(value, node.left);
+        } else {
+            return Search(value, node.right);
         }
-        return null;
     }
+
+    @Override
+    public int compare(T o1, T o2) {
+        return o1.compareTo(o2);
+    }
+
+
 }
