c77447f5f950aec79f2ecd2e1c6ccba6f681d00d
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-02-21 08:42:49 +0000

    tests BRS

--- dsa/src/test/java/commonsense/tree/graph/VertexTest.java
+++ dsa/src/test/java/commonsense/tree/graph/VertexTest.java
@@ -114,61 +114,61 @@ class VertexTest {
         // create vertices
         Vertex<Integer> v1 = new Vertex<>(1);
         Vertex<Integer> v2 = new Vertex<>(2);
-//        Vertex<Integer> v3 = new Vertex<>(3);
-//        Vertex<Integer> v4 = new Vertex<>(4);
-//        Vertex<Integer> v5 = new Vertex<>(5);
-//        Vertex<Integer> v6 = new Vertex<>(6);
-//        Vertex<Integer> v7 = new Vertex<>(7);
-//        Vertex<Integer> v8 = new Vertex<>(8);
-//        Vertex<Integer> v9 = new Vertex<>(9);
+        Vertex<Integer> v3 = new Vertex<>(3);
+        Vertex<Integer> v4 = new Vertex<>(4);
+        Vertex<Integer> v5 = new Vertex<>(5);
+        Vertex<Integer> v6 = new Vertex<>(6);
+        Vertex<Integer> v7 = new Vertex<>(7);
+        Vertex<Integer> v8 = new Vertex<>(8);
+        Vertex<Integer> v9 = new Vertex<>(9);
 
-        // add adjacent vertices
-//        v1.addAdjacentVertex(v2);
-//        v1.addAdjacentVertex(v3);
-//        v1.addAdjacentVertex(v4);
-//        v1.addAdjacentVertex(v5);
-//
-//        v2.addAdjacentVertex(v6);
-//        v6.addAdjacentVertex(v8);
-//        v8.addAdjacentVertex(v3);
-//
-//        v3.addAdjacentVertex(v8);
-//
-//
-//        v4.addAdjacentVertex(v7);
-//        v7.addAdjacentVertex(v9);
-//
-//        v5.addAdjacentVertex(v4);
+//         add adjacent vertices
+        v1.addAdjacentVertex(v2);
+        v1.addAdjacentVertex(v3);
+        v1.addAdjacentVertex(v4);
+        v1.addAdjacentVertex(v5);
 
-//        MatcherAssert.assertThat(v1.breathFirstSearch(1), Matchers.equalTo(v1));
-//        MatcherAssert.assertThat(v2.breathFirstSearch(1), Matchers.equalTo(v1));
-//        MatcherAssert.assertThat(v3.breathFirstSearch(1), Matchers.equalTo(v1));
-//        MatcherAssert.assertThat(v4.breathFirstSearch(1), Matchers.equalTo(v1));
-//        MatcherAssert.assertThat(v5.breathFirstSearch(1), Matchers.equalTo(v1));
+        v2.addAdjacentVertex(v6);
+        v6.addAdjacentVertex(v8);
+        v8.addAdjacentVertex(v3);
 
-//        MatcherAssert.assertThat(v1.breathFirstSearch(2), Matchers.equalTo(v2));
-//        MatcherAssert.assertThat(v2.breathFirstSearch(2), Matchers.equalTo(v2));
-//        MatcherAssert.assertThat(v3.breathFirstSearch(2), Matchers.equalTo(v2));
-//        MatcherAssert.assertThat(v4.breathFirstSearch(2), Matchers.equalTo(v2));
-//        MatcherAssert.assertThat(v5.breathFirstSearch(2), Matchers.equalTo(v2));
-//
-//
-//        MatcherAssert.assertThat(v1.breathFirstSearch(3), Matchers.equalTo(v3));
-//        MatcherAssert.assertThat(v1.breathFirstSearch(4), Matchers.equalTo(v4));
-//        MatcherAssert.assertThat(v1.breathFirstSearch(5), Matchers.equalTo(v5));
+        v3.addAdjacentVertex(v8);
+
+
+        v4.addAdjacentVertex(v7);
+        v7.addAdjacentVertex(v9);
+
+        v5.addAdjacentVertex(v4);
+
+        MatcherAssert.assertThat(v1.breathFirstSearch(1), Matchers.equalTo(v1));
+        MatcherAssert.assertThat(v2.breathFirstSearch(1), Matchers.equalTo(v1));
+        MatcherAssert.assertThat(v3.breathFirstSearch(1), Matchers.equalTo(v1));
+        MatcherAssert.assertThat(v4.breathFirstSearch(1), Matchers.equalTo(v1));
+        MatcherAssert.assertThat(v5.breathFirstSearch(1), Matchers.equalTo(v1));
+
+        MatcherAssert.assertThat(v1.breathFirstSearch(2), Matchers.equalTo(v2));
+        MatcherAssert.assertThat(v2.breathFirstSearch(2), Matchers.equalTo(v2));
+        MatcherAssert.assertThat(v3.breathFirstSearch(2), Matchers.equalTo(v2));
+        MatcherAssert.assertThat(v4.breathFirstSearch(2), Matchers.equalTo(v2));
+        MatcherAssert.assertThat(v5.breathFirstSearch(2), Matchers.equalTo(v2));
+
+
+        MatcherAssert.assertThat(v1.breathFirstSearch(3), Matchers.equalTo(v3));
+        MatcherAssert.assertThat(v1.breathFirstSearch(4), Matchers.equalTo(v4));
+        MatcherAssert.assertThat(v1.breathFirstSearch(5), Matchers.equalTo(v5));
     }
 
 
     @Test
     public void breathFirstSearchTraversal() {
-            // create vertices
-            Vertex<Integer> v1 = new Vertex<>(1);
-            Vertex<Integer> v2 = new Vertex<>(2);
-            Vertex<Integer> v3 = new Vertex<>(3);
-            Vertex<Integer> v4 = new Vertex<>(4);
-            Vertex<Integer> v5 = new Vertex<>(5);
-            Vertex<Integer> v6 = new Vertex<>(6);
-            Vertex<Integer> v7 = new Vertex<>(7);
+        // create vertices
+        Vertex<Integer> v1 = new Vertex<>(1);
+        Vertex<Integer> v2 = new Vertex<>(2);
+        Vertex<Integer> v3 = new Vertex<>(3);
+        Vertex<Integer> v4 = new Vertex<>(4);
+        Vertex<Integer> v5 = new Vertex<>(5);
+        Vertex<Integer> v6 = new Vertex<>(6);
+        Vertex<Integer> v7 = new Vertex<>(7);
 
 
             /*
@@ -180,28 +180,28 @@ class VertexTest {
                    / \
                   v5 v7
              */
-            // add adjacent vertices
-            v1.addAdjacentVertex(v2);
-            v1.addAdjacentVertex(v3);
-            v2.addAdjacentVertex(v4);
-            v3.addAdjacentVertex(v4);
-            v3.addAdjacentVertex(v6);
-            v4.addAdjacentVertex(v5);
-            v4.addAdjacentVertex(v7);
+        // add adjacent vertices
+        v1.addAdjacentVertex(v2);
+        v1.addAdjacentVertex(v3);
+        v2.addAdjacentVertex(v4);
+        v3.addAdjacentVertex(v4);
+        v3.addAdjacentVertex(v6);
+        v4.addAdjacentVertex(v5);
+        v4.addAdjacentVertex(v7);
 
-            // set system out to new stream
-            PrintStream origOutStream = System.out;
-            final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            System.setOut(new PrintStream(outputStream));
+        // set system out to new stream
+        PrintStream origOutStream = System.out;
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
 
-            // DO work - traverse the graph
-            v1.breathFirstSearchTraverse();
-            String expected = "1\n2\n3\n4\n6\n5\n7\n";
-            assertEquals(expected, outputStream.toString());
+        // DO work - traverse the graph
+        v1.breathFirstSearchTraverse();
+        String expected = "1\n2\n3\n4\n6\n5\n7\n";
+        assertEquals(expected, outputStream.toString());
 
 
-            // reset system out
-            System.setOut(origOutStream);
-        }
+        // reset system out
+        System.setOut(origOutStream);
+    }
 
 }
