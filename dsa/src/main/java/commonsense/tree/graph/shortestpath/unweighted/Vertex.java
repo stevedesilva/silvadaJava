3fe6531dd4990bc6e497571770735b257a5f7fda
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-07-14 09:46:14 +0100

    add test for unweighted shortest path

---
+++ dsa/src/main/java/commonsense/tree/graph/shortestpath/unweighted/Vertex.java
@@ -0,0 +1,138 @@
+package commonsense.tree.graph.shortestpath.unweighted;
+
+import java.util.*;
+
+public class Vertex<T> {
+    final int id = UUID.randomUUID().hashCode();
+    T value;
+    List<Vertex<T>> edges;
+
+    public Vertex(){}
+
+    public Vertex(T value) {
+        this.value = value;
+        this.edges = new ArrayList<>();
+    }
+
+    public void addAdjacentVertex(Vertex<T> vertex) {
+       if(edges.contains(vertex)) {
+           return;
+       }
+       edges.add(vertex);
+       vertex.addAdjacentVertex(this);
+    }
+
+    public Vertex<T> dfs(T value) {
+        return dfs(value, this, new HashMap<Vertex<T>, Boolean>());
+    }
+
+    private Vertex<T> dfs(T value, Vertex<T> vertex, Map<Vertex<T>,Boolean> visited ) {
+        // return if
+        if (vertex.value.equals(value)) {
+            return vertex;
+        }
+        visited.put(vertex,true);
+
+        for (Vertex<T> adjacentVertx : vertex.edges) {
+            if(!visited.containsKey(adjacentVertx)) {
+                if (adjacentVertx.value.equals(value)) {
+                    return adjacentVertx;
+                }
+                Vertex<T> result = dfs(value, adjacentVertx, visited);
+                if (result != null) {
+                    return result;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    // dfs_traversal
+    public void dfsTraverse() {
+        // create a map to store visited vertex
+        Map<Vertex<T>, Boolean> visited = new HashMap<>();
+
+        // call dfsTraverse recursively
+        dfsTraverse(this, visited);
+    }
+
+    private void dfsTraverse(Vertex<T> vertex, Map<Vertex<T>, Boolean> visited) {
+        // if vertex is already visited mark it as true
+        visited.put(vertex, true);
+
+        // print the vertex
+        System.out.println(vertex.value);
+
+        // for each adjacent vertex of the vertex
+        for (Vertex<T> adjacent : vertex.edges) {
+            // if adjacent vertex is already visited
+            if (!visited.containsKey(adjacent)) {
+                // call dfsTraverse recursively
+                dfsTraverse(adjacent, visited);
+            }
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vertex<?> vertex = (Vertex<?>) o;
+        return id == vertex.id && Objects.equals(value, vertex.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, value);
+    }
+
+    public void breathFirstSearchTraverse() {
+        // create a map to store visited vertex
+        Map<Vertex<T>, Boolean> visited = new HashMap<>();
+        // java queue
+        Queue<Vertex<T>> queue = new LinkedList<>();
+
+        // call dfsTraverse recursively
+        bfsTraverse(this, visited, queue);
+    }
+
+    private void bfsTraverse(Vertex<T> vertex, Map<Vertex<T>, Boolean> visited, Queue<Vertex<T>> queue) {
+        visited.put(vertex, true);
+        queue.add(vertex);
+        while (!queue.isEmpty()) {
+            final Vertex<T> current = queue.remove();
+            System.out.println(current.value);
+            for (Vertex<T> v : current.edges) {
+                if (!visited.containsKey(v)) {
+                    visited.put(v, true);
+                    queue.add(v);
+                }
+            }
+        }
+    }
+
+    public Vertex<T> breathFirstSearch(T value) {
+        Map<Vertex<T>, Boolean> visited = new HashMap<>();
+        Queue<Vertex<T>> queue = new LinkedList<>();
+        visited.put(this, true);
+        queue.add(this);
+        while (!queue.isEmpty()) {
+            final Vertex<T> current = queue.remove();
+            if(current.value.equals(value)){
+                return current;
+            }
+            for (Vertex<T> v : current.edges) {
+                if (!visited.containsKey(v)) {
+                    visited.put(v, true);
+                    queue.add(v);
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<T> shortestPath(Vertex<T> start, Vertex<T> end) {
+        return List.of();
+    }
+}
---
+++ dsa/src/test/java/commonsense/tree/graph/shortestpath/unweighted/VertexTest.java
@@ -0,0 +1,61 @@
+package commonsense.tree.graph.shortestpath.unweighted;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VertexTest {
+
+    @Test
+    void shortestPath() {
+    }
+
+    @Test
+    public void testShortestPath() {
+
+        // create vertices
+        Vertex<String> idris = new Vertex<>("idris");
+        Vertex<String> kamil = new Vertex<>("kamil");
+        Vertex<String> lina = new Vertex<>("lina");
+        Vertex<String> sasha = new Vertex<>("sasha");
+        Vertex<String> marco = new Vertex<>("marco");
+        Vertex<String> ken = new Vertex<>("ken");
+        Vertex<String> talia = new Vertex<>("talia");
+
+        // add adjacent vertices
+        idris.addAdjacentVertex(kamil);
+        idris.addAdjacentVertex(talia);
+
+        kamil.addAdjacentVertex(idris);
+        kamil.addAdjacentVertex(lina);
+
+        lina.addAdjacentVertex(kamil);
+        lina.addAdjacentVertex(sasha);
+
+        sasha.addAdjacentVertex(lina);
+        sasha.addAdjacentVertex(marco);
+
+        marco.addAdjacentVertex(sasha);
+        marco.addAdjacentVertex(ken);
+
+        ken.addAdjacentVertex(marco);
+        ken.addAdjacentVertex(talia);
+
+        talia.addAdjacentVertex(ken);
+        talia.addAdjacentVertex(idris);
+
+
+        // given a graph
+        Vertex<String> graph = new Vertex<>();
+
+        // when we find the shortest path
+        graph.shortestPath(idris, lina);
+        // then we should get the shortest path
+        List<String> expected = List.of("idris", "kamil", "lina");
+        MatcherAssert.assertThat(graph.shortestPath(idris, lina), Matchers.equalTo(expected));
+    }
+}
