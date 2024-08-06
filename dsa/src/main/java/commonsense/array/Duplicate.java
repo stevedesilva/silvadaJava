49b7dd94f926f373fac885ba5d31f8605814c458
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-08-06 23:43:16 +0100

    fix  unweighted shortest path

--- dsa/src/main/java/commonsense/array/Duplicate.java
+++ dsa/src/main/java/commonsense/array/Duplicate.java
@@ -11,4 +11,57 @@ public class Duplicate {
         }
         return false;
     }
+    /*
+    func FindDuplicates(array []string) bool {
+	table := make(map[string]bool)
+	for _, v := range array {
+		if _, ok := table[v]; ok {
+			return true
+		}
+		table[v] = true
+	}
+	return false
 }
+*/
+    public boolean hasDuplicatesWithMap(int[] array) {
+        java.util.Map<Integer, Boolean> table = new java.util.HashMap<>();
+        for (int v : array) {
+            if (table.containsKey(v)) {
+                return true;
+            }
+            table.put(v, true);
+        }
+        return false;
+    }
+    /*
+
+func FindDuplicatesWithSort(array []string) bool {
+	// sort array
+	sort.Strings(array)
+
+	// compare adjacent elements
+	for i := 0; i < len(array)-1; i++ {
+		if array[i] == array[i+1] {
+			return true
+		}
+	}
+	return false
+}
+     */
+
+    public boolean hasDuplicatesWithSort(int[] array) {
+        // sort array
+        java.util.Arrays.sort(array);
+
+        // compare adjacent elements
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] == array[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
+
--- dsa/src/test/java/commonsense/tree/graph/shortestpath/unweighted/VertexTest.java
+++ dsa/src/test/java/commonsense/tree/graph/shortestpath/unweighted/VertexTest.java
@@ -58,4 +58,51 @@ class VertexTest {
         List<String> expected = List.of("idris", "kamil", "lina");
         MatcherAssert.assertThat(graph.shortestPath(idris, lina), Matchers.equalTo(expected));
     }
+
+    @Test
+    public void testShortestPathIdris() {
+
+        // create vertices
+        Vertex<String> idris = new Vertex<>("idris");
+        Vertex<String> kamil = new Vertex<>("kamil");
+        Vertex<String> lina = new Vertex<>("lina");
+        Vertex<String> sasha = new Vertex<>("sasha");
+        Vertex<String> marco = new Vertex<>("marco");
+        Vertex<String> ken = new Vertex<>("ken");
+        Vertex<String> talia = new Vertex<>("talia");
+
+        // add adjacent vertices
+        idris.addAdjacentVertex(kamil);
+        idris.addAdjacentVertex(talia);
+        idris.addAdjacentVertex(lina);
+
+        kamil.addAdjacentVertex(idris);
+        kamil.addAdjacentVertex(lina);
+
+        lina.addAdjacentVertex(kamil);
+        lina.addAdjacentVertex(sasha);
+
+        sasha.addAdjacentVertex(lina);
+        sasha.addAdjacentVertex(marco);
+
+        marco.addAdjacentVertex(sasha);
+        marco.addAdjacentVertex(ken);
+
+        ken.addAdjacentVertex(marco);
+        ken.addAdjacentVertex(talia);
+
+        talia.addAdjacentVertex(ken);
+        talia.addAdjacentVertex(idris);
+
+
+        // given a graph
+        Vertex<String> graph = new Vertex<>();
+
+        // when we find the shortest path
+        graph.shortestPath(idris, lina);
+        // then we should get the shortest path
+        List<String> expected = List.of("idris", "lina");
+        MatcherAssert.assertThat(graph.shortestPath(idris, lina), Matchers.equalTo(expected));
+    }
+
 }
