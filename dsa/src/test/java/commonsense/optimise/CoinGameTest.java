1c6714e3bfb53b550db744338263574618a60772
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-09-12 09:24:15 +0100

    create tests

--- dsa/src/test/java/commonsense/optimise/CoinGameTest.java
+++ dsa/src/test/java/commonsense/optimise/CoinGameTest.java
@@ -12,17 +12,41 @@ class CoinGameTest {
         coinGame = new CoinGame();
     }
 
+    /*
+        Player turn ME
+        Number of coins = 1 Winner = THEM
+        Number of coins = 2 Winner = ME
+        Number of coins = 3 Winner = ME
+        Number of coins = 4 Winner = THEM
+        Number of coins = 5 Winner = ME
+        Number of coins = 6 Winner = ME
+        Number of coins = 7 Winner = THEM
+        Number of coins = 8 Winner = ME
+        Number of coins = 9 Winner = ME
+        Number of coins = 10 Winner = THEM
+     */
     @Test
     public void testPlayOneCoin() {
         // Test when "me" should win
         assertEquals(CoinGame.THEM, coinGame.play(1, CoinGame.ME));
-        assertEquals(CoinGame.ME, coinGame.play(1, CoinGame.THEM));
     }
 
     @Test
     public void testPlayTwoCoin() {
         // Test when "me" should win
         assertEquals(CoinGame.ME, coinGame.play(2, CoinGame.ME));
-        assertEquals(CoinGame.THEM, coinGame.play(2, CoinGame.THEM));
     }
+
+    @Test
+    public void testPlayThree() {
+        // Test when "me" should win
+        assertEquals(CoinGame.ME, coinGame.play(3, CoinGame.ME));
+    }
+
+    @Test
+    public void testPlayFour() {
+        // Test when "me" should win
+        assertEquals(CoinGame.THEM, coinGame.play(4, CoinGame.ME));
+    }
+
 }
