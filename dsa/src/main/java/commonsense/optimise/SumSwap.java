8db952872be5207b4a60ce5d6da5f115d2024d28
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-12-11 22:20:52 +0000

    validate input

--- dsa/src/main/java/commonsense/optimise/SumSwap.java
+++ dsa/src/main/java/commonsense/optimise/SumSwap.java
@@ -4,12 +4,19 @@ import java.util.List;
 
 public class SumSwap {
 
-
     public int[] swapToMakeEqual(int[] a1, int[] a2) {
+        validate(a1, a2);
+
+        return null;
+    }
+
+    private void validate(int[] a1, int[] a2) {
         // validation
         if (a1 == null || a2 == null) {
             throw new IllegalArgumentException("input arrays cannot be null");
         }
-        return null;
+        if (a1.length == 0 || a2.length == 0) {
+            throw new IllegalArgumentException("input arrays cannot be empty");
+        }
     }
 }
