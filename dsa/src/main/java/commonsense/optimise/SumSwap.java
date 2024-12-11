a2784e6fabf9e1168dccfd8a44013b6a8e59f60f
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2024-12-11 22:20:52 +0000

    add more tests

--- dsa/src/main/java/commonsense/optimise/SumSwap.java
+++ dsa/src/main/java/commonsense/optimise/SumSwap.java
@@ -7,6 +7,9 @@ public class SumSwap {
 
     public int[] swapToMakeEqual(int[] a1, int[] a2) {
         // validation
+        if (a1 == null || a2 == null) {
+            throw new IllegalArgumentException("input arrays cannot be null");
+        }
         return null;
     }
 }
