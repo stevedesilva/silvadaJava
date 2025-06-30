f35ad535e2146d4c79dcadf552cd33b37483c948
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-06-30 08:01:01 +0100

    add increasing triplets sum

--- dsa/src/main/java/commonsense/optimise/Stock.java
+++ dsa/src/main/java/commonsense/optimise/Stock.java
@@ -2,7 +2,7 @@ package commonsense.optimise;
 
 public class Stock {
     // stock prediction
-    public boolean predictor(int[] stockPrices) {
+    public boolean isIncreasingTriplet(int[] stockPrices) {
         return false;
     }
 }
--- dsa/src/test/java/commonsense/optimise/StockTest.java
+++ dsa/src/test/java/commonsense/optimise/StockTest.java
@@ -1,7 +1,20 @@
 package commonsense.optimise;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StockTest {
-
+    // stock prediction
+    @ParameterizedTest
+    @CsvSource(value = {
+            "'5,2,8,4,3,7',true",
+    })
+    public void predictorTest() {
+        Stock stock = new Stock();
+        int[] stockPrices = {100, 200, 150, 300, 250};
+        boolean result = stock.isIncreasingTriplet(stockPrices);
+        assertFalse(result); // Placeholder assertion; actual logic needs to be implemented
+    }
 }
