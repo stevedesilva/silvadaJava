f36b7755a7a92c17f4adedbf688b9c1e2958876f
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-08-19 08:01:09 +0100

    add stock prediction code

--- dsa/src/main/java/commonsense/optimise/StockPrediction.java
+++ dsa/src/main/java/commonsense/optimise/StockPrediction.java
@@ -3,6 +3,23 @@ package commonsense.optimise;
 public class StockPrediction {
 
     public static double findFindGreatestProfit(double[] numbers) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        double low = numbers[0];
+        double high = numbers[0];
+        double total = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            double current = numbers[i];
+            if (current > high) {
+                high = current;
+            } else if (current < low) {
+                // lowest point so start again
+                low = current;
+                high= current;
+            }
+            double currentTotal = high - low;
+            if (currentTotal > total) {
+                total = currentTotal;
+            }
+        }
+        return total;
     }
 }
