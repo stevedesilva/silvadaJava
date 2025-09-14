c51370d54406c6c1f50f5493583562dd5157d7c6
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-09-14 10:55:17 +0100

    add test for find he product

--- dsa/src/main/java/commonsense/optimise/MaxProduct.java
+++ dsa/src/main/java/commonsense/optimise/MaxProduct.java
@@ -4,14 +4,33 @@ public class MaxProduct {
 
 
     public int findMaxProductInArray(int[] nums) {
-        int maxProduct = 0;
-        int currentSum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            // if current sum + number is less that 0 then reset current sum to 0
-            currentSum = Math.max(currentSum + nums[i], 0);
-            // if current sum is greater than max sum then set max sum to current sum
-            maxProduct = Math.max(maxProduct, currentSum);
+        if (nums == null || nums.length < 1) {
+            throw new IllegalArgumentException("input array cannot be less than 1 element");
         }
-        return maxProduct;
+
+        int maxValue1 = -Integer.MAX_VALUE;
+        int maxValue2= -Integer.MAX_VALUE;
+        int leastValue1 = Integer.MAX_VALUE;
+        int leastValue2 = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            // find the two largest values
+            if (nums[i] > maxValue1) {
+                maxValue2 = maxValue1;
+                maxValue1 = nums[i];
+            } else if (nums[i] > maxValue2) {
+                maxValue2 = nums[i];
+            }
+            // find the two smallest values
+            if (nums[i] < leastValue1) {
+                leastValue2 = leastValue1;
+                leastValue1 = nums[i];
+            } else if (nums[i] < leastValue2) {
+                leastValue2 = nums[i];
+            }
+        }
+
+        return Math.max(maxValue1 * maxValue2, leastValue1 * leastValue2);
+
     }
 }
--- dsa/src/test/java/commonsense/optimise/MaxProductTest.java
+++ dsa/src/test/java/commonsense/optimise/MaxProductTest.java
@@ -9,8 +9,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class MaxProductTest {
 
     @ParameterizedTest
-    @CsvSource({})
-    void findMaxProductInArray() {
+    @CsvSource(value = {
+            "'3,-1,1',3",
+            "'1,2,4,5',20",
+            "'-1,1,1,1',1",
+            "'-10,1,7,-9,1',90",
+            "'-1,-2,-3',6",
+            "'0,2,3',6",
+            "'-1,0,1',0",
+    })
+    void findMaxProductInArray(String a1, int expected) {
+        MaxProduct maxProduct = new MaxProduct();
+        int[] a1Array = convertStringToIntArray(a1);
+        final int result = maxProduct.findMaxProductInArray(a1Array);
+        assertEquals(expected, result);
+    }
+    private int[] convertStringToIntArray(String a1) {
+        String[] split = a1.split(",");
+        int[] result = new int[split.length];
+        for (int i = 0; i < split.length; i++) {
+            result[i] = Integer.parseInt(split[i]);
+        }
+        return result;
+    }
 
+    @Test
+    void testEmptyArray() {
+        MaxProduct maxProduct = new MaxProduct();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            maxProduct.findMaxProductInArray(new int[]{});
+        });
+        assertEquals("input array cannot be less than 1 element", exception.getMessage());
     }
 }
+
