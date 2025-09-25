ad80b884c765a37f5f97b70eda73eaaa7568451f
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-09-25 09:31:16 +0100

    add test for find he product

---
+++ dsa/src/main/java/commonsense/optimise/LongestSequence.java
@@ -0,0 +1,9 @@
+package commonsense.optimise;
+
+public class LongestSequence {
+
+    public int[] findLongestConsecutiveSequence(int[] nums) {
+        // Implementation goes here
+        return null;
+    }
+}
