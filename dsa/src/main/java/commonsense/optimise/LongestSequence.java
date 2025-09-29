87c5bfa5fec2f3b5d6b9b7d34533e4c89de90310
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-09-29 08:44:16 +0100

    fix test

--- dsa/src/main/java/commonsense/optimise/LongestSequence.java
+++ dsa/src/main/java/commonsense/optimise/LongestSequence.java
@@ -1,9 +1,30 @@
 package commonsense.optimise;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LongestSequence {
 
     public static int[] findLongestConsecutiveSequence(int[] nums) {
         // Implementation goes here
+        Map numMap = new HashMap<>();
+        for (int num : nums) {
+            numMap.put(num, true);
+        }
+        // find the longest sequence
+        // if num - 1 is not in the map then it is the start of a sequence
+        List result = new ArrayList();
+        for (int num: nums) {
+
+            if (!numMap.containsKey(num + 1)) {
+                // start of sequence
+
+            }
+        }
+
+        //
         return null;
     }
 }
--- dsa/src/test/java/commonsense/optimise/LongestSequenceTest.java
+++ dsa/src/test/java/commonsense/optimise/LongestSequenceTest.java
@@ -14,7 +14,12 @@ public class LongestSequenceTest {
     @CsvSource(value = {
             "1:1",
             "1,2:1,2",
-            "1,2,3:1,2,3"
+            "1,2,3:1,2,3",
+            "1,2,4,5:4,5",
+            "10,5,12,3,55,30,4,11,2,6:4,5,6",
+            "100,4,200,1,3,2:1,2,3,4",
+            "0,-1: -1,0",
+            "9,1,4,7,3,-1,0,5,6,8:3,4,5,6,7,8,9"
     }, delimiter = ':')
     public void TestLongestSequence(String input, String output) {
         final int[] numbers = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
